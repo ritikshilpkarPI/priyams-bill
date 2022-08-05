1b01ef17b34f99320a5cc32a5fe370214f24b8fc
Yash priyam <yashpriyam74@gmail.com>
2022-08-05 21:19:35 +0530

    add dropdown

--- src/Components/BillFeed.jsx
+++ src/Components/BillFeed.jsx
@@ -1,4 +1,5 @@
-import { Table, Text } from "@mantine/core";
+import { useState } from "react";
+import { Table, Text, Collapse } from "@mantine/core";
 
 export const BillFeed = ({ bills = [] }) => {
   return (
@@ -30,95 +31,106 @@ export const BillFeed = ({ bills = [] }) => {
       </thead>
       <tbody className="body">
         {bills.map((item, idx) => {
-          return (
-            <tr className="bill-row" key={`${item}$${idx}`}>
-              <td>
-                <Text color="black" weight={500}>
-                  {idx + 1}
-                </Text>
-              </td>
-              <td>
-                <Text color="black" weight={500}>
-                  {item["billAmountTotal"]}
-                </Text>
-              </td>
-              <td>
-                <Text color="black" weight={500}>
-                  {item["billMRPTotal"]}
-                </Text>
-              </td>
-              <td>
-                <Text color="black" weight={500}>
-                  {item["totalNumberOfItems"]}
-                </Text>
-              </td>
-              <td>
-                <Text color="black" weight={500}>
-                  {item["totalNumberOfUniqueItems"]}
-                </Text>
-              </td>
-              <td>
-                <Text color="black" weight={500}>
-                  {new Date(item["createdAt"]).toLocaleString()}
-                </Text>
-              </td>
-              <td>
-                <Table>
-                  <thead className="heading">
-                    <tr>
-                      <th>
-                        <Text>Sl. No.</Text>
-                      </th>
-                      <th>
-                        <Text>Item name</Text>
-                      </th>
-                      <th>
-                        <Text>Item Quantity</Text>
-                      </th>
-                      <th>
-                        <Text>Item Total Amount</Text>
-                      </th>
-                    </tr>
-                  </thead>
-                  <tbody className="body">
-                    {item.items.map((itemObj, idx) => {
-                      const {
-                        itemDetail,
-                        itemQuantityInBill,
-                        itemSellingPriceTotal,
-                      } = itemObj;
-                      return (
-                        <tr key={idx}>
-                          <td>
-                            <Text color="black" weight={500}>
-                              {idx + 1}
-                            </Text>
-                          </td>
-                          <td>
-                            <Text color="black" weight={500}>
-                              {itemDetail?.itemName || "Item name not found"}
-                            </Text>
-                          </td>
-                          <td>
-                            <Text color="black" weight={500}>
-                              {itemQuantityInBill}
-                            </Text>
-                          </td>
-                          <td>
-                            <Text color="black" weight={500}>
-                              {itemSellingPriceTotal}
-                            </Text>
-                          </td>
-                        </tr>
-                      );
-                    })}
-                  </tbody>
-                </Table>
-              </td>
-            </tr>
-          );
+          return <TableRow item={item} idx={idx} />;
         })}
       </tbody>
     </Table>
+  );
+};
+
+const TableRow = ({ item, idx }) => {
+  const [open, setOpen] = useState(false);
+  return (
+    <tr
+      onClick={() => setOpen(!open)}
+      className="bill-row"
+      key={`${item}$${idx}`}
+    >
+      <td>
+        <Text color="black" weight={500}>
+          {idx + 1}
+        </Text>
+      </td>
+      <td>
+        <Text color="black" weight={500}>
+          {item["billAmountTotal"]}
+        </Text>
+      </td>
+      <td>
+        <Text color="black" weight={500}>
+          {item["billMRPTotal"]}
+        </Text>
+      </td>
+      <td>
+        <Text color="black" weight={500}>
+          {item["totalNumberOfItems"]}
+        </Text>
+      </td>
+      <td>
+        <Text color="black" weight={500}>
+          {item["totalNumberOfUniqueItems"]}
+        </Text>
+      </td>
+      <td>
+        <Text color="black" weight={500}>
+          {new Date(item["createdAt"]).toLocaleString()}
+        </Text>
+      </td>
+      <td>
+        <Collapse in={open}>
+          <Table>
+            <thead className="heading">
+              <tr>
+                <th>
+                  <Text>Sl. No.</Text>
+                </th>
+                <th>
+                  <Text>Item name</Text>
+                </th>
+                <th>
+                  <Text>Item Quantity</Text>
+                </th>
+                <th>
+                  <Text>Item Total Amount</Text>
+                </th>
+              </tr>
+            </thead>
+            <tbody className="body">
+              {item.items.map((itemObj, idx) => {
+                const {
+                  itemDetail,
+                  itemQuantityInBill,
+                  itemSellingPriceTotal,
+                } = itemObj;
+                return (
+                  <tr key={idx}>
+                    <td>
+                      <Text color="black" weight={500}>
+                        {idx + 1}
+                      </Text>
+                    </td>
+                    <td>
+                      <Text color="black" weight={500}>
+                        {itemDetail?.itemName || "Item name not found"}
+                      </Text>
+                    </td>
+                    <td>
+                      <Text color="black" weight={500}>
+                        {itemQuantityInBill}
+                      </Text>
+                    </td>
+                    <td>
+                      <Text color="black" weight={500}>
+                        {itemSellingPriceTotal}
+                      </Text>
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </Table>
+        </Collapse>
+      </td>
+    </tr>
   );
 };
