dfe77f04091dd3d7954feab1b0d596635daa836a
Yash priyam <yashpriyam74@gmail.com>
2022-09-25 17:39:40 +0530

    remove comment code

--- server/routes/item-routes.js
+++ server/routes/item-routes.js
@@ -1,23 +1,18 @@
 const itemRoutes = require("express").Router();
-// const multer = require("multer");
-// const upload = multer({ dest: "uploads/" });
 
 const {
   getItemsFeed,
   addItems,
   editItemById,
   softDeleteItem,
-  addBulkItems
+  addBulkItems,
 } = require("../controllers/item-controller");
-
-// const upload = require('../middleware/upload');
 
 // itemRoutes.post("/newbill", userSignupValidator, runValidation, userSignup);
 itemRoutes.get("/items", getItemsFeed);
 itemRoutes.post("/addNewItem", addItems);
 itemRoutes.put("/editItemById", editItemById);
 itemRoutes.post("/softDeleteItem", softDeleteItem);
-// itemRoutes.post("/addbulkitems", upload.single('file'), addBulkItems);
 itemRoutes.post("/addbulkitems", addBulkItems);
 
 module.exports = itemRoutes;
