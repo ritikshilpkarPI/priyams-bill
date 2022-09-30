918588e2e82de1a668e7ad1b750f8e0d3e084d2e
Yash priyam <yashpriyam74@gmail.com>
2022-09-30 15:20:47 +0530

    add error response status

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,43 +1,61 @@
-const user = require('../data/user');
-const jwt = require('jsonwebtoken');
+const user = require("../data/user");
+const jwt = require("jsonwebtoken");
 
 const loginUser = async (request, response) => {
-    try {
-        // Getting user input details
-        let { username, password } = await request.body;
+  try {
+    // Getting user input details
+    let { username, password } = await request.body;
 
-        // Our saved database user 
-        let userData = user.userData.filter((item) => item.username === username);
+    // Our saved database user
+    let userData = user.userData.filter((item) => item.username === username);
 
-        if (userData.length === 0) {
-            // If email not registered
-            return response.status(200).json({ status: false, message: 'invalid username' });
-        } else if (userData[0].password != password) {
-            // If password don't match
-            return response.status(200).json({ status: false, message: 'wrong password' });
-        } else if (userData[0].username === username && userData[0].password === password) {
-            // JWT Token
-            const token = await jwt.sign({ username: userData[0].username, role: userData[0].role }, process.env.JSON_WEB_TOKEN_SECRET);
+    if (userData.length === 0) {
+      // If email not registered
+      return response
+        .status(200)
+        .json({ status: false, message: "invalid username" });
+    } else if (userData[0].password != password) {
+      // If password don't match
+      return response
+        .status(200)
+        .json({ status: false, message: "wrong password" });
+    } else if (
+      userData[0].username === username &&
+      userData[0].password === password
+    ) {
+      // JWT Token
+      const token = await jwt.sign(
+        { username: userData[0].username, role: userData[0].role },
+        process.env.JSON_WEB_TOKEN_SECRET
+      );
 
-            console.log(token);
+      console.log(token);
 
-            // Sending cookie
-            // response.cookie('authtoken', token);
+      // Sending cookie
+      // response.cookie('authtoken', token);
 
-            return response.status(200).json({ status: true, message: 'login successfull', authtoken: token, role: userData[0].role, name: userData[0].name });
-        }
-    } catch (error) {
-        response.status.json(error);
+      return response
+        .status(200)
+        .json({
+          status: true,
+          message: "login successfull",
+          authtoken: token,
+          role: userData[0].role,
+          name: userData[0].name,
+        });
     }
-}
+  } catch (error) {
+    response.status(501).json(error);
+  }
+};
 
 const logoutUser = async (request, response) => {
-    try {
-        response.clearCookie('authtoken');
-        response.status(200).json({ status: true, message: 'logout user' });
-    } catch (error) {
-        response.status(500).json(error);
-    }
-}
+  try {
+    response.clearCookie("authtoken");
+    response.status(200).json({ status: true, message: "logout user" });
+  } catch (error) {
+    response.status(500).json(error);
+  }
+};
 
 module.exports = { loginUser, logoutUser };
