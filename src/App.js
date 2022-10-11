a1dd950577714881198d4810f5220126bcec9616
Yash priyam <yashpriyam74@gmail.com>
2022-10-11 12:23:20 +0530

    fix login

--- src/App.js
+++ src/App.js
@@ -1,26 +1,14 @@
-import './CSS/App.scss';
-
-import {
-  lazy,
-  Suspense,
-  useContext,
-  useEffect,
-  useState,
-} from 'react';
-
-import {
-  Route,
-  Switch,
-  withRouter,
-} from 'react-router-dom';
-
-import { SegmentedControl } from '@mantine/core';
-
-import { AppStateContext } from './AppState/appState.context';
-import CustomerBill from './Pages/CustomerBill';
-import { Axios } from './utils/axios';
+import "./CSS/App.scss";
+import { useContext, useEffect, useState, lazy, Suspense } from "react";
+import { Route, Switch, withRouter } from "react-router-dom";
+import { Axios } from "./utils/axios";
+import { SegmentedControl, Button } from "@mantine/core";
+import { AppStateContext } from "./AppState/appState.context";
+import CustomerBill from "./Pages/CustomerBill";
+import ProtectedRoutes from "./components/ProtectedRoutes";
 
 // import ProtectedRoutes from "./components/ProtectedRoutes";
+const Home = lazy(() => import("./Pages/Home"));
 const BillFeed = lazy(() => import("./Pages/BillFeed"));
 const Billing = lazy(() => import("./Pages/Billing"));
 const DayWiseBillFeed = lazy(() => import("./Pages/DailyBill"));
@@ -28,7 +16,7 @@ const EditBill = lazy(() => import("./Pages/EditBill"));
 const ItemsList = lazy(() => import("./Pages/ItemsList"));
 const OpenClose = lazy(() => import("./Pages/OpenClose"));
 const StockQuantity = lazy(() => import("./Pages/StockQuantity"));
-// const Login = lazy(() => import("./Pages/Login"));
+const Login = lazy(() => import("./Pages/Login"));
 const MiscellaneousExpenses = lazy(() =>
   import("./Pages/MiscellaneousExpenses")
 );
@@ -36,7 +24,7 @@ const MiscellaneousExpenses = lazy(() =>
 // import { QRComp } from "./qr";
 
 const PAGES = {
-  "/": "Home",
+  Home: "Home",
   billing: "Billing",
   inventory: "Inventory",
   allBill: "All Bills",
@@ -52,10 +40,15 @@ function App({ history, location }) {
   const [itemsList, dispatch] = itemsStateAndDispatch;
   const [loaderDisplay, setLoaderDisplay] = useState(true);
   const [value, setValue] = useState(showBill ? {} : Object.keys(PAGES)[1]);
-  // const staffName = JSON.parse(localStorage.getItem("priyam-store"))?.name;
-  // const staffUserName = JSON.parse(
-  //   localStorage.getItem("priyam-store")
-  // )?.username;
+  const staffName = JSON.parse(localStorage.getItem("priyam-store"))?.name;
+  const staffUserName = JSON.parse(
+    localStorage.getItem("priyam-store")
+  )?.username;
+
+  // useEffect(() => {
+  //   setValue((prev) => prev);
+  // }, []);
+
   useEffect(() => {
     (async () => {
       const fetch = await Axios.request({
@@ -76,56 +69,70 @@ function App({ history, location }) {
       setLoaderDisplay(false);
     })();
   }, [dispatch]);
+
   useEffect(() => {
     if (!showBill) {
-      history.push(value);
+      history.push(`/${value}`);
     }
-  }, [value, history, showBill]);
+    // eslint-disable-next-line
+  }, [value, showBill]);
 
-  // const logoutUser = async () => {
-  //   // const fetch = await Axios.request({
-  //   //   url: "/api/auth/logout",
-  //   //   method: "get",
-  //   // });
+  const logoutUser = async () => {
+    // const fetch = await Axios.request({
+    //   url: "/api/auth/logout",
+    //   method: "get",
+    // });
 
-  //   // if (fetch.data.status === true && fetch.data.message === "logout user") {
-  //   localStorage.removeItem("priyam-store");
-  //   history.push("/login");
-  //   // }
-  // };
+    // if (fetch.data.status === true && fetch.data.message === "logout user") {
+    localStorage.removeItem("priyam-store");
+    history.push("/login");
+    // }
+  };
 
   console.log({ itemsList });
 
   return (
-    <>
-      <div className="App">
+    <div className="App">
+      {localStorage.getItem("priyam-store") && !showBill && (
         <div className="nav-btn">
-          {!showBill ? (
-            <SegmentedControl
-              value={value}
-              onChange={setValue}
-              color="blue"
-              radius="md"
-              size="md"
-              data={Object.keys(PAGES).map((page) => ({
-                label: PAGES[page],
-                value: page,
-              }))}
-            />
-          ) : (
-            ""
-          )}
+          <SegmentedControl
+            value={value}
+            onChange={setValue}
+            color="blue"
+            radius="md"
+            size="md"
+            data={Object.keys(PAGES).map((page) => ({
+              label: PAGES[page],
+              value: page,
+            }))}
+          />
+          <Button className="logout-btn" onClick={logoutUser}>
+            Logout
+          </Button>
         </div>
-        <Suspense fallback={<div>Loading...</div>}>
-          <Switch>
-            <Route path="/openClose" component={OpenClose} />
-            <Route
-              exact
-              path="/showBill/:customerBillId"
-              component={CustomerBill}
-            />
+      )}
+
+      {staffName && <h3 className="staffname">{staffName}</h3>}
+      {staffUserName && <p className="staffname">{staffUserName}</p>}
+
+      <Suspense fallback={<div>Loading...</div>}>
+        <Switch>
+          <Route path="/login" exact component={Login} />
+          {/* <Route
+            path="/login"
+            exact
+            render={() => <Login reload={setValue} />}
+            // render={<Login />}
+          /> */}
+          <Route
+            exact
+            path="/showBill/:customerBillId"
+            component={CustomerBill}
+          />
+          <ProtectedRoutes>
             <Route
               path="/billing"
+              exact
               render={() => (
                 <Billing
                   loaderDisplay={loaderDisplay}
@@ -133,17 +140,20 @@ function App({ history, location }) {
                 />
               )}
             />
-            <Route path="/inventory" component={ItemsList} />
-            <Route path="/dayBill" component={DayWiseBillFeed} />
-            <Route path="/expenses" component={MiscellaneousExpenses} />
-            <Route path="/stockquantity" component={StockQuantity} />
-            <Route path="/allBill" component={BillFeed} />
-            <Route path="/:billingID" component={EditBill} />
-          </Switch>
-        </Suspense>
-        {/* <QRComp /> */}
-      </div>
-    </>
+            <Route path="/openClose" component={OpenClose} />
+
+            <Route exact path="/inventory" component={ItemsList} />
+            <Route exact path="/" component={Home} />
+            <Route exact path="/dayBill" component={DayWiseBillFeed} />
+            <Route exact path="/expenses" component={MiscellaneousExpenses} />
+            <Route exact path="/stockquantity" component={StockQuantity} />
+            <Route exact path="/allBill" component={BillFeed} />
+          </ProtectedRoutes>
+          <Route exact path="/edit/:billingID" component={EditBill} />
+        </Switch>
+      </Suspense>
+      {/* <QRComp /> */}
+    </div>
   );
 }
 
