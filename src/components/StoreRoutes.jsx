ab86e655a659f8921908fbe42e1191b1e1b676db
Hs809 <hiteshpal.8097@gmail.com>
2022-10-16 00:19:19 +0530

    created routes,constant and customhooks for app file

---
+++ src/components/StoreRoutes.jsx
@@ -0,0 +1,61 @@
+import React from 'react';
+import { Route, Switch } from "react-router-dom";
+import { Suspense } from "react";
+import CustomerBill from "../Pages/CustomerBill";
+import ProtectedRoutes from "../components/ProtectedRoutes";
+import ProtectedComponent from "../components/ProtectedComponent";
+import access from "../access";
+import {
+    Home,
+    BillFeed,
+    Billing,
+    DayWiseBillFeed,
+    EditBill,
+    ItemsList,
+    OpenClose,
+    StockQuantity,
+    Report,
+    Login
+} from '../Pages'
+
+const StoreRoutes = ({ loaderDisplay, setLoaderDisplay }) => {
+    return (
+        <Suspense fallback={<div>Loading...</div>}>
+            <Switch>
+                <Route path="/login" exact component={Login} />
+                <Route
+                    exact
+                    path="/showBill/:customerBillId"
+                    component={CustomerBill}
+                />
+                <ProtectedRoutes>
+                    <Route
+                        path="/billing"
+                        exact
+                        render={() => (
+                            <Billing
+                                loaderDisplay={loaderDisplay}
+                                setLoaderDisplay={setLoaderDisplay}
+                            />
+                        )}
+                    />
+                    <Route path="/openClose" component={OpenClose} />
+
+                    <Route exact path="/inventory" component={ItemsList} />
+                    <Route exact path="/" component={Home} />
+                    <ProtectedComponent role={access.DAY_BILL_ROUTE}>
+                        <Route exact path="/dayBill" component={DayWiseBillFeed} />
+                    </ProtectedComponent>
+                    <Route exact path="/stockquantity" component={StockQuantity} />
+                    <Route exact path="/allBill" component={BillFeed} />
+                    <ProtectedComponent role={access.REPORT_PAGE_ROUTE}>
+                        <Route exact path="/report" component={Report} />
+                    </ProtectedComponent>
+                    <Route exact path="/edit/:billingID" component={EditBill} />
+                </ProtectedRoutes>
+            </Switch>
+        </Suspense>
+    )
+
+}
+export default StoreRoutes;
---
+++ src/routes/index.js
@@ -0,0 +1,15 @@
+import { include } from 'named-urls'
+
+export const routes = {
+    home: "/",
+    login: "/login",
+    showBill: "/showBill/:customerBillId",
+    billing: "/billing",
+    openClose: "/openClose",
+    inventory: "/inventory",
+    dayBill: "/dayBill",
+    stockQuantity: "/stockquantity",
+    allBills: "/allBill",
+    billReport: "/report",
+    editBill: "/edit/:billingID"
+}
