ddc5b347ffce3a83b2ea4948dda1bbdd9ade3cbd
Yash priyam <yashpriyam74@gmail.com>
2022-11-04 18:53:59 +0530

    remove commented code

--- src/Pages/Attendance.jsx
+++ src/Pages/Attendance.jsx
@@ -1,142 +1,155 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
-import { Table, Loader, Title } from '@mantine/core'
-import { DateRangePicker } from '@mantine/dates';
+import { Table, Loader, Title } from "@mantine/core";
+import { DateRangePicker } from "@mantine/dates";
 
 function msToTime(duration) {
-    const seconds = duration / 1000;
-    const hours = seconds / 3600
-    return Math.floor(hours);
+  const seconds = duration / 1000;
+  const hours = seconds / 3600;
+  return Math.floor(hours);
 }
 
 const Attendance = () => {
-    const [attendance, setAttendance] = useState([])
-    const [dateWiseAttendance, setDateWiseAttendance] = useState([])
-    const [loader, setLoader] = useState(false);
-    const [loader2, setLoader2] = useState(false)
-    const [open, setOpen] = useState(false)
-    const [name, setname] = useState("");
-    const options = { year: 'numeric', month: 'numeric', day: 'numeric' }
-    const region = "en-US"
-    const [value, setValue] = useState([
-        new Date(),
-        new Date(),
-    ])
-    const getAttendance = async () => {
-        setLoader(true)
-        try {
-            const monthlyattendance = await axios.request({
-                url: "/api/attendance/monthlyAttendance",
-                method: "get",
-            })
-            setAttendance(monthlyattendance.data.message)
-            setLoader(false)
-        } catch (error) {
-            console.log(error);
-        }
+  const [attendance, setAttendance] = useState([]);
+  const [dateWiseAttendance, setDateWiseAttendance] = useState([]);
+  const [loader, setLoader] = useState(false);
+  const [loader2, setLoader2] = useState(false);
+  const [open, setOpen] = useState(false);
+  const [name, setname] = useState("");
+  const options = { year: "numeric", month: "numeric", day: "numeric" };
+  const region = "en-US";
+  const [value, setValue] = useState([new Date(), new Date()]);
+  const getAttendance = async () => {
+    setLoader(true);
+    try {
+      const monthlyattendance = await axios.request({
+        url: "/api/attendance/monthlyAttendance",
+        method: "get",
+      });
+      setAttendance(monthlyattendance.data.message);
+      setLoader(false);
+    } catch (error) {
+      console.error(error);
     }
-    const getDateWiseAttendance = async (name) => {
-        setname(name)
-        setLoader2(true)
-        try {
-            const result = await axios.request({
-                url: `/api/attendance/dailyAttendance`,
-                method: "post",
-                data: {
-                    startDate: value[0].toLocaleDateString(region, options),
-                    endDate: value[1].toLocaleDateString(region, options),
-                    name
-                }
-            });
-            setDateWiseAttendance(result.data.message)
-        } catch (error) {
-            console.log(error);
-        }
-        setLoader2(false)
+  };
+  const getDateWiseAttendance = async (name) => {
+    setname(name);
+    setLoader2(true);
+    try {
+      const result = await axios.request({
+        url: `/api/attendance/dailyAttendance`,
+        method: "post",
+        data: {
+          startDate: value[0].toLocaleDateString(region, options),
+          endDate: value[1].toLocaleDateString(region, options),
+          name,
+        },
+      });
+      setDateWiseAttendance(result.data.message);
+    } catch (error) {
+      console.error(error);
     }
-    useEffect(() => {
-        getAttendance()
-    }, [])
-    const rows2 = dateWiseAttendance.map((element, index) => (
-        <tr key={index}>
-            <td>{index + 1}</td>
-            <td>{new Date(element.date).toLocaleDateString("en-US", { year: 'numeric', month: 'numeric', day: 'numeric' })}</td>
-            <td>{element.attendance ? "Present" : "Absent"}</td>
-            <td>{new Date(element.arrivingTime).toLocaleTimeString()}</td>
-            <td>{new Date(element.leavingTime).toLocaleTimeString()}</td>
-            <td>{msToTime(element.totalHoursOfWork)}</td>
-            <td>{element.workHoursCompleted ? "Completed" : "Not Completed"}</td>
-        </tr>
-    ))
-    const rows = attendance.map((element, index) => (
-        <tr key={index} onClick={() => {
-            getDateWiseAttendance(element._id);
-            setOpen(!open)
-        }}>
-            <td>{index + 1}</td>
-            <td>{element._id}</td>
-            <td>{element.workingDays}</td>
-            <td>{msToTime(element.workingHours)}</td>
-            <td>{element.holidays}</td>
-        </tr>
-    ));
+    setLoader2(false);
+  };
+  useEffect(() => {
+    getAttendance();
+  }, []);
+  const rows2 = dateWiseAttendance.map((element, index) => (
+    <tr key={index}>
+      <td>{index + 1}</td>
+      <td>
+        {new Date(element.date).toLocaleDateString("en-US", {
+          year: "numeric",
+          month: "numeric",
+          day: "numeric",
+        })}
+      </td>
+      <td>{element.attendance ? "Present" : "Absent"}</td>
+      <td>{new Date(element.arrivingTime).toLocaleTimeString()}</td>
+      <td>{new Date(element.leavingTime).toLocaleTimeString()}</td>
+      <td>{msToTime(element.totalHoursOfWork)}</td>
+      <td>{element.workHoursCompleted ? "Completed" : "Not Completed"}</td>
+    </tr>
+  ));
+  const rows = attendance.map((element, index) => (
+    <tr
+      key={index}
+      onClick={() => {
+        getDateWiseAttendance(element._id);
+        setOpen(!open);
+      }}
+    >
+      <td>{index + 1}</td>
+      <td>{element._id}</td>
+      <td>{element.workingDays}</td>
+      <td>{msToTime(element.workingHours)}</td>
+      <td>{element.holidays}</td>
+    </tr>
+  ));
 
-    return (
-        <div className="attendance-wrapper">
-            <DateRangePicker
-                label="Select Date"
-                placeholder="Pick dates range"
-                value={value}
-                onChange={setValue}
-                className="date-picker"
-            />
-            {
-                loader ? <Loader color="blue" size="lg" /> :
-
-                    <Table highlightOnHover withBorder withColumnBorders>
-                        <thead>
-                            <tr>
-                                <th>SR NO</th>
-                                <th>Staff Name</th>
-                                <th>Total Working Days</th>
-                                <th>Total Working Hours </th>
-                                <th>Holidays</th>
-                            </tr>
-                        </thead>
-                        <tbody>{rows}</tbody>
-                    </Table>
-            }
-            {
-                loader2 ? <Title order={3}>Data is fetching for {name}</Title> : ''
-            }
-            {
-                loader2 ?
-                    <Loader color="blue" size="lg" /> :
-                    !dateWiseAttendance.length ? <>
-                        <Title className="table-title" order={2}>No Data is present for {name} on this Date</Title>
-                        <p>Try to choose different dates</p>
-                    </> :
-                        <>
-                            <Title className="table-title" order={3}>Viewing Attendance for {name}  </Title>
-                            <Table className="attendance-table" highlightOnHover withBorder withColumnBorders>
-                                <thead>
-                                    <tr>
-                                        <th>SR NO</th>
-                                        <th>Date</th>
-                                        <th>Present/Absent</th>
-                                        <th>In Time</th>
-                                        <th>Out Time</th>
-                                        <th>Total Working Hours</th>
-                                        <th>Work Completed</th>
-                                    </tr>
-                                </thead>
-                                <tbody>{rows2}</tbody>
-                            </Table>
-
-                        </>
-            }
-        </div>
-    )
-}
+  return (
+    <div className="attendance-wrapper">
+      <DateRangePicker
+        label="Select Date"
+        placeholder="Pick dates range"
+        value={value}
+        onChange={setValue}
+        className="date-picker"
+      />
+      {loader ? (
+        <Loader color="blue" size="lg" />
+      ) : (
+        <Table highlightOnHover withBorder withColumnBorders>
+          <thead>
+            <tr>
+              <th>SR NO</th>
+              <th>Staff Name</th>
+              <th>Total Working Days</th>
+              <th>Total Working Hours </th>
+              <th>Holidays</th>
+            </tr>
+          </thead>
+          <tbody>{rows}</tbody>
+        </Table>
+      )}
+      {loader2 ? <Title order={3}>Data is fetching for {name}</Title> : ""}
+      {loader2 ? (
+        <Loader color="blue" size="lg" />
+      ) : !dateWiseAttendance.length ? (
+        <>
+          <Title className="table-title" order={2}>
+            No Data is present for {name} on this Date
+          </Title>
+          <p>Try to choose different dates</p>
+        </>
+      ) : (
+        <>
+          <Title className="table-title" order={3}>
+            Viewing Attendance for {name}{" "}
+          </Title>
+          <Table
+            className="attendance-table"
+            highlightOnHover
+            withBorder
+            withColumnBorders
+          >
+            <thead>
+              <tr>
+                <th>SR NO</th>
+                <th>Date</th>
+                <th>Present/Absent</th>
+                <th>In Time</th>
+                <th>Out Time</th>
+                <th>Total Working Hours</th>
+                <th>Work Completed</th>
+              </tr>
+            </thead>
+            <tbody>{rows2}</tbody>
+          </Table>
+        </>
+      )}
+    </div>
+  );
+};
 
 export default Attendance;
--- src/components/EmployeeAttendance.jsx
+++ src/components/EmployeeAttendance.jsx
@@ -1,127 +1,139 @@
-import React from 'react';
-import { Group, Title, Select, Button } from '@mantine/core';
-import { useState } from 'react';
-import Axios from 'axios';
+import React from "react";
+import { Group, Title, Select, Button } from "@mantine/core";
+import { useState } from "react";
+import Axios from "axios";
 const nameOptionAndValues = [
-    { value: 'anjali', label: 'Anjali' },
-    { value: 'abhishek', label: 'Abhishek' },
-    { value: 'rajesh', label: 'Rajesh' },
-]
+  { value: "anjali", label: "Anjali" },
+  { value: "abhishek", label: "Abhishek" },
+  { value: "rajesh", label: "Rajesh" },
+];
 const attendanceOptions = [
-    { value: 'arrival', label: "Arrival" },
-    { value: 'leave', label: "Leaving" }
-]
+  { value: "arrival", label: "Arrival" },
+  { value: "leave", label: "Leaving" },
+];
 const presentAbsentOptions = [
-    { value: 'present', label: "Present" },
-    { value: 'absent', label: "Absent" }
-]
-const totalWorkHoursInMillis = 40680000
+  { value: "present", label: "Present" },
+  { value: "absent", label: "Absent" },
+];
+const totalWorkHoursInMillis = 40680000;
 const EmployeeAttendance = () => {
-    const [name, setName] = useState("");
-    const [state, setState] = useState("arrival");
-    const [attendance, setAttendance] = useState("present")
+  const [name, setName] = useState("");
+  const [state, setState] = useState("arrival");
+  const [attendance, setAttendance] = useState("present");
 
-    const handleAttendance = async () => {
-        // Checks if the staff has marks the attendance for arrival and stops him 
-        // for doing it again.
-        const date = new Date();
-        const dateString = date.toLocaleDateString("en-US", { year: 'numeric', month: 'numeric', day: 'numeric' })
-        const attendee = JSON.parse(localStorage.getItem(name));
-        if (!name) {
-            alert("Select the name for attendance")
-            return;
-        } else if (name === attendee?.name && state === "arrival") {
-            alert(`You have already made attendance for ${name} Arrival `)
-        } else if (state === "absent") {
-            try {
-                await Axios.request({
-                    url: `/api/attendance/markAbsent`,
-                    method: "post",
-                    data: {
-                        name,
-                        date: dateString,
-                    }
-                });
-                alert(`You have successfully marked Absent  for ${name} `)
-            } catch (error) {
-                console.log(error)
-            }
-        } else if (name !== attendee?.name && state === "arrival") {
-            try {
-                const result = await Axios.request({
-                    url: `/api/attendance/dailyAttendanceArrival`,
-                    method: "post",
-                    data: {
-                        name,
-                        arrivingTime: date,
-                        date: dateString,
-                        attendance: attendance === "present" ? true : false
-                    }
-                });
-                if (result.status === 200) {
-                    localStorage.setItem(result.data.message.name, JSON.stringify(result.data.message));
-                    alert(`You have successfully marked the Arrival attendance for ${name} `)
-                } else {
-                    alert(result.data.message)
-                }
-            } catch (error) {
-                console.log(error)
-            }
-        } else if (name === attendee?.name && state === "leave") {
-            try {
-                let totalHours = date.getTime() - new Date(attendee.arrivingTime).getTime();
-                await Axios.request({
-                    url: `/api/attendance/dailyAttendanceLeaving`,
-                    method: "post",
-                    data: {
-                        id: attendee._id,
-                        attendanceToBeUpdated: {
-                            arrivingTime: attendee.arrivingTime,
-                            name: attendee.name,
-                            leavingTime: date,
-                            date: attendee.date,
-                            totalHoursOfWork: totalHours,// Saving total hours in milliseconds
-                            workHoursCompleted: totalHours >= totalWorkHoursInMillis
-                        }
-                    }
-                });
-                alert(`You have successfully marked the Leaving attendance for ${name} `)
-            } catch (error) {
-                console.log(error);
-            }
+  const handleAttendance = async () => {
+    // Checks if the staff has marks the attendance for arrival and stops him
+    // for doing it again.
+    const date = new Date();
+    const dateString = date.toLocaleDateString("en-US", {
+      year: "numeric",
+      month: "numeric",
+      day: "numeric",
+    });
+    const attendee = JSON.parse(localStorage.getItem(name));
+    if (!name) {
+      alert("Select the name for attendance");
+      return;
+    } else if (name === attendee?.name && state === "arrival") {
+      alert(`You have already made attendance for ${name} Arrival `);
+    } else if (state === "absent") {
+      try {
+        await Axios.request({
+          url: `/api/attendance/markAbsent`,
+          method: "post",
+          data: {
+            name,
+            date: dateString,
+          },
+        });
+        alert(`You have successfully marked Absent  for ${name} `);
+      } catch (error) {
+        console.error(error);
+      }
+    } else if (name !== attendee?.name && state === "arrival") {
+      try {
+        const result = await Axios.request({
+          url: `/api/attendance/dailyAttendanceArrival`,
+          method: "post",
+          data: {
+            name,
+            arrivingTime: date,
+            date: dateString,
+            attendance: attendance === "present" ? true : false,
+          },
+        });
+        if (result.status === 200) {
+          localStorage.setItem(
+            result.data.message.name,
+            JSON.stringify(result.data.message)
+          );
+          alert(
+            `You have successfully marked the Arrival attendance for ${name} `
+          );
         } else {
-            alert(`You need to add arriving Data first for ${name} `)
+          alert(result.data.message);
         }
-        setName("")
-    };
-    return (
-        <div className='attendance-container'>
-            <Title order={2} >Attendance</Title>
-            <Group align="center" position="apart">
-                <Select
-                    label="Select your Name"
-                    placeholder="Pick one"
-                    data={nameOptionAndValues}
-                    value={name}
-                    onChange={(value) => setName(value)}
-                />
-                <Select
-                    label="Select Arrival/Leaving"
-                    placeholder="Pick one"
-                    data={attendanceOptions}
-                    onChange={(value) => setState(value)}
-                    defaultValue={state}
-                />
-                <Select
-                    label="Select Present/Absent"
-                    placeholder="Pick one"
-                    data={presentAbsentOptions}
-                    onChange={(value) => setAttendance(value)}
-                    defaultValue={attendance}
-                />
-                <Button onClick={handleAttendance}>Submit</Button>
-            </Group>
-        </div>
-    )
-}
+      } catch (error) {
+        console.error(error);
+      }
+    } else if (name === attendee?.name && state === "leave") {
+      try {
+        let totalHours =
+          date.getTime() - new Date(attendee.arrivingTime).getTime();
+        await Axios.request({
+          url: `/api/attendance/dailyAttendanceLeaving`,
+          method: "post",
+          data: {
+            id: attendee._id,
+            attendanceToBeUpdated: {
+              arrivingTime: attendee.arrivingTime,
+              name: attendee.name,
+              leavingTime: date,
+              date: attendee.date,
+              totalHoursOfWork: totalHours, // Saving total hours in milliseconds
+              workHoursCompleted: totalHours >= totalWorkHoursInMillis,
+            },
+          },
+        });
+        alert(
+          `You have successfully marked the Leaving attendance for ${name} `
+        );
+      } catch (error) {
+        console.error(error);
+      }
+    } else {
+      alert(`You need to add arriving Data first for ${name} `);
+    }
+    setName("");
+  };
+  return (
+    <div className="attendance-container">
+      <Title order={2}>Attendance</Title>
+      <Group align="center" position="apart">
+        <Select
+          label="Select your Name"
+          placeholder="Pick one"
+          data={nameOptionAndValues}
+          value={name}
+          onChange={(value) => setName(value)}
+        />
+        <Select
+          label="Select Arrival/Leaving"
+          placeholder="Pick one"
+          data={attendanceOptions}
+          onChange={(value) => setState(value)}
+          defaultValue={state}
+        />
+        <Select
+          label="Select Present/Absent"
+          placeholder="Pick one"
+          data={presentAbsentOptions}
+          onChange={(value) => setAttendance(value)}
+          defaultValue={attendance}
+        />
+        <Button onClick={handleAttendance}>Submit</Button>
+      </Group>
+    </div>
+  );
+};
 export default EmployeeAttendance;
