93ba9e963bb023305b14eed3d9cdeea89c0f6894
Hs809 <hiteshpal.8097@gmail.com>
2022-11-09 18:16:47 +0530

    added search customer by mobile no and name on bill

--- server/controllers/bill-controller.js
+++ server/controllers/bill-controller.js
@@ -263,6 +263,28 @@ const sendMessage = async (req, res) => {
     res.status(500).json({ error: error.message });
   }
 };
+const userDetails = async (req, res) => {
+  try {
+    const userDetails = await Bill.aggregate([
+      {
+        $match: {
+          customerPhone: {
+            $ne: null
+          }
+        }
+      },
+      {
+        $group: {
+          _id: "$customerPhone",
+          customerName: { "$first": "$customerName" }
+        }
+      }
+    ])
+    res.status(200).json({ message: userDetails });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+}
 
 module.exports = {
   addNewBill,
@@ -271,4 +293,5 @@ module.exports = {
   getEditBill,
   editBill,
   sendMessage,
+  userDetails
 };
