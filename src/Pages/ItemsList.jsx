62c22a097337c1af0e87e6e604d4e5ca85f73dce
Hs809 <hiteshpal.8097@gmail.com>
2022-11-10 12:04:30 +0530

    added serial number column in inventory table

--- src/Pages/ItemsList.jsx
+++ src/Pages/ItemsList.jsx
@@ -956,6 +956,9 @@ const ItemsList = () => {
         className="table-row"
       >
         <td className="table-row-component">
+          {index + 1}
+        </td>
+        <td className="table-row-component">
           <BarcodeRow style={style} index={index} />
         </td>
         <td>
@@ -1288,6 +1291,9 @@ const ItemsList = () => {
           <thead className="heading">
             <tr className="table-row">
               <th className="table-head-component">
+                <Text>SR No</Text>
+              </th>
+              <th className="table-head-component">
                 <Text>Bar Code</Text>
                 <input
                   type="checkbox"
@@ -1470,6 +1476,7 @@ const ItemsList = () => {
             className="add-item-row-body"
           >
             <tr className="bill-row">
+              <td></td>
               <td>
                 <Input
                   value={newItemInput["itemBarcode"]}
