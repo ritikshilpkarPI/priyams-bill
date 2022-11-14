c49cefeb883ccf997e10a7ff718047fa86c1b6c7
Yash priyam <yashpriyam74@gmail.com>
2022-11-14 16:06:48 +0530

    no refresh needed after inventory update

--- server/controllers/item-controller.js
+++ server/controllers/item-controller.js
@@ -2,18 +2,15 @@ const { Item } = require("../db-models/item-model");
 
 const getItemsFeed = async (req, res) => {
   try {
-    const reqFilters = JSON.parse(req.query.filters);
-    let items = [];
-    if (reqFilters.isDeleted === false) {
-      items = await Item.find({}, null, { sort: { itemName: 1 } });
+    const { minStockOnly = false, isDeleted = false } = JSON.parse(
+      req.query.filters
+    );
+    let items = await Item.find({}, null, { sort: { itemName: 1 } });
+    if (isDeleted === false) {
       items = items.filter((item) => !item.isDeleted);
     }
-    if (reqFilters.minStockOnly) {
-      items = await Item.find({
-        minStockReached: reqFilters.minStockOnly,
-        isDeleted: reqFilters.isDeleted,
-      }).sort({ itemStockQuantity: 1 });
-      items = items.filter((item) => !item.isDeleted);
+    if (minStockOnly) {
+      items = items.filter((item) => item.minStockReached);
     }
     const itemCount = items.length;
     res.status(200).json({ message: { items, itemCount } });
