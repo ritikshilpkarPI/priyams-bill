11e89eee3881420f5ef66292fd5014cb528644b9
Hs809 <hiteshpal.8097@gmail.com>
2022-11-17 16:14:58 +0530

    coorected the quantity in edit bill and updated the with CRUD items when the bill is edit

--- src/Pages/Billing.jsx
+++ src/Pages/Billing.jsx
@@ -214,7 +214,7 @@ async function addNewBill(
   };
   const objectOfInterest = billID ? editApi : createApi;
 
-  await Axios.request({
+  const response = await Axios.request({
     ...objectOfInterest,
     headers: {
       Cookie: "",
@@ -222,7 +222,7 @@ async function addNewBill(
   });
   window.print();
   setApiLoading(false);
-  setBill(BILL_INITIAL_STATE);
+  setBill(BILL_INITIAL_STATE)
   itemsReducer({ type: "UPDATE_ITEMS_LIST", payload: [...initialItemList] });
 }
 
@@ -302,7 +302,6 @@ const Billing = ({ billID = "", loaderDisplay }) => {
   const [filterUserProfile, setFilterUserProfile] = useState([]);
   const [showProfileData, setShowProfileData] = useState(false);
   // const [loaderDisplay, setLoaderDisplay] = loaderState;
-
   const getUserData = async () => {
     try {
       const response = await axios.get("/api/billing/userDetails");
@@ -322,7 +321,6 @@ const Billing = ({ billID = "", loaderDisplay }) => {
     });
     setFilterUserProfile(users);
   };
-
   // To refresh page
 
   // To save bill items in billItem Reducer
@@ -858,7 +856,7 @@ const Billing = ({ billID = "", loaderDisplay }) => {
               </td>
             </tr>
             {bill.billItems.map((item, idx) => {
-              const itemObj = { ...item, ...item.itemDetail };
+              const itemObj = { ...item.itemDetail, ...item };
               return (
                 <tr
                   className="bill-item-row"
@@ -1231,7 +1229,6 @@ const QuantBtn = ({ itemObj, idx, bill, setBill }) => {
     billItemsCopy[idx]["itemQuantityInBill"] = Number(e.target.value);
     setBill((prev) => ({ ...prev, billItems: [...billItemsCopy] }));
   };
-
   return (
     <>
       <Text
