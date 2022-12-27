c398d1aec2ff88adc87c24abba5ffcf80b9fe224
Hs809 <hiteshpal.8097@gmail.com>
2022-12-27 19:45:14 +0530

    remove fetch and add axios in approval

--- src/Pages/Approval.jsx
+++ src/Pages/Approval.jsx
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import React, { useEffect, useState } from 'react'
 import PurchaseDetailsApproval from 'src/components/PurchaseApproval/PurchaseDetailsApproval';
 
@@ -6,34 +7,44 @@ const Approval = () => {
   useEffect(() => {
     callAPI();
   }, []);
-  const callAPI = () => {
-    fetch("/api/purchaseOrder/orders")
-      .then((res) => res.json())
-      .then((data) => {
-        let pendingArray = [];
-        let approvedArray = [];
-        let rejectedArray = [];
-        let totalArray = data.orders;
-        const role = JSON.parse(localStorage.getItem("priyam-store")).role;
-        for (let i = 0; i < totalArray.length; i++) {
-          if (role === 'admin') {
-            if (totalArray[i].isRejected) {
-              rejectedArray.push(totalArray[i]);
-            } else if (totalArray[i].isApproved) {
-              approvedArray.push(totalArray[i]);
-            } else if (totalArray[i].isDraft) {
-              pendingArray.push(totalArray[i]);
-            }
+  const callAPI = async () => {
+
+    try {
+      const { data } = await axios.get("/api/purchaseOrder/orders")
+      console.log({ data });
+
+      let pendingArray = [];
+      let approvedArray = [];
+      let rejectedArray = [];
+      let totalArray = data.orders;
+      const role = JSON.parse(localStorage.getItem("priyam-store")).role;
+      for (let i = 0; i < totalArray.length; i++) {
+        if (role === 'admin') {
+          if (totalArray[i].isRejected) {
+            rejectedArray.push(totalArray[i]);
+          } else if (totalArray[i].isApproved) {
+            approvedArray.push(totalArray[i]);
           } else {
-            if (totalArray[i].isRejected) {
-              rejectedArray.push(totalArray[i]);
-            } else if (!totalArray[i].isApproved) {
-              pendingArray.push(totalArray[i]);
-            }
+            pendingArray.push(totalArray[i]);
+          }
+        } else {
+          if (totalArray[i].isRejected) {
+            rejectedArray.push(totalArray[i]);
+          } else if (!totalArray[i].isApproved) {
+            pendingArray.push(totalArray[i]);
           }
         }
-        setList([...pendingArray, ...rejectedArray, ...approvedArray]);
-      })
+      }
+      console.log({ pendingArray, approvedArray, rejectedArray });
+      setList([...pendingArray, ...rejectedArray, ...approvedArray]);
+
+    } catch (error) {
+      console.error(error)
+    }
+    // fetch("/api/purchaseOrder/orders")
+    //   .then((res) => res.json())
+    //   .then((data) => {
+    //   })
   }
 
   return (
