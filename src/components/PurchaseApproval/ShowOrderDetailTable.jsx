5c90ae83606dec3ac5358e59e23ee16a0252d5e6
Hs809 <hiteshpal.8097@gmail.com>
2023-01-16 16:07:10 +0530

    edited-tested-text

--- src/components/PurchaseApproval/ShowOrderDetailTable.jsx
+++ src/components/PurchaseApproval/ShowOrderDetailTable.jsx
@@ -40,7 +40,7 @@ const ShowOrderDetailTable = ({ purchaseList}) => {
           <Table withColumnBorders striped withBorder>
             <thead>
               <tr>
-                <th>Barcode-barcode</th>
+                <th>Barcode</th>
                 <th>Item name</th>
                 <th>Order Quantity</th>
                 <th>Minimum Quantity</th>
@@ -50,7 +50,7 @@ const ShowOrderDetailTable = ({ purchaseList}) => {
                 <th>MRP</th>
                 <th>Cost Price</th>
                 <th>Expiry Dates</th>
-                <th>Remarks123</th>
+                <th>Remarks</th>
               </tr>
             </thead>
             <tbody>{rows}</tbody>
