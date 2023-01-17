1fb094b5bcac9233598125b78c618746179257ed
Hs809 <hiteshpal.8097@gmail.com>
2023-01-17 18:56:16 +0530

    multiple-labels-search-filter-deletion

--- src/Pages/Label.jsx
+++ src/Pages/Label.jsx
@@ -1,19 +1,20 @@
 import React, { useState, useEffect } from 'react'
-import { Loader,Button } from '@mantine/core';
+import { Loader, Button } from '@mantine/core';
+import { CloseButton } from '@mantine/core';
 
 const Label = () => {
     const [value, setValue] = useState("");
     const [data, setData] = useState([]);
     const [loader, setLoader] = useState(false);
     const [render, setRender] = useState(false);
-    const [disp, setDisp] = useState(true);
+    const [display, setDisplay] = useState(true);
     const [newData, setNewData] = useState([]);
     const [checked, setChecked] = useState(false);
     const getData = () => {
         setLoader(true);
-        fetch('https://dev--priyams.netlify.app/.netlify/functions/app/api/inventory/items?filters=%7B%22minStockOnly%22:false,%22isDeleted%22:false%7D').then((response) => response.json()).then((data) => {
+        fetch(`${process.env.REACT_APP_LABEL_API}`).then((response) => response.json()).then((data) => {
             setData(data.message.items)
-            setDisp(true);
+            setDisplay(true);
             setLoader(false);
         });
 
@@ -24,30 +25,44 @@ const Label = () => {
     }, []);
     useEffect(() => {
         const myData = data.filter(function (item) {
-            return item.itemName === value;
+            // eslint-disable-next-line  
+            return (item.itemName === value || item.itemBarcode == value);
         });
-        setNewData(myData);
-        setDisp(false);
-      // eslint-disable-next-line  
+
+        if (myData.length) {
+            setNewData([...newData, myData[0]]);
+            setDisplay(false);
+        }
+        // eslint-disable-next-line  
     }, [render])
+
     const onChange = (event) => {
         setValue(event.target.value);
     };
+ 
 
     const onSearch = (searchTerm) => {
+        searchTerm = String(searchTerm);
         setValue(searchTerm);
-        setRender(!render)
+        let toggle = !render;
+        setRender(toggle)
         console.log("search ", searchTerm);
     };
+    
     const handleChange = () => {
         setChecked(!checked);
     };
+    const handleRemoveItem=(idx)=>{
+        console.log(idx);
+        setNewData(newData.filter((item,index) => index !== idx));
+    }
    
+    var regExp = /[a-zA-Z]/g;
     return (
         <>
             <div className="search-container">
                 <div className="search-inner">
-                    <input type="text" value={value} onChange={onChange} className='input-item-search'/>
+                    <input type="text" placeholder='Item Name' value={value} onChange={onChange} className='input-item-search' />
                     <Button onClick={() => onSearch(value)} className='search-btn'> Search </Button>
                     <label for="myCheck" className='label'>Alignment</label>
                     <input type="checkbox" id="myCheck" checked={checked}
@@ -56,14 +71,27 @@ const Label = () => {
                 <div className='dropdown'>
                     {data
                         .filter((item) => {
-                            const searchTerm = value.toLowerCase();
-                            const itemName = item.itemName.toLowerCase();
+                            if (regExp.test(value)) {
+                                const searchTerm = value.toLowerCase();
+                                const itemName = item.itemName.toLowerCase();
 
-                            return (
-                                searchTerm &&
-                                itemName.includes(searchTerm) &&
-                                itemName !== searchTerm
-                            );
+                                return (
+                                    searchTerm &&
+                                    itemName.includes(searchTerm) &&
+                                    itemName !== searchTerm
+                                );
+                            }
+                            else {
+                                const searchTerm = String(value);
+                                const itemBarcode = String(item.itemBarcode);
+    
+                                return (
+                                    searchTerm &&
+                                    itemBarcode.includes(searchTerm) &&
+                                    itemBarcode !== searchTerm
+                                );
+                            }
+
                         })
                         .slice(0, 10)
                         .map((item) => (
@@ -72,9 +100,13 @@ const Label = () => {
                                 className="dropdown-row"
                                 key={item.itemName}
                             >
-                                {item.itemName}
+                                <span className='search-item-name-dropdown'> {item.itemBarcode}</span> 
+                                <span >  {item.itemName} </span>
+                                <hr/>
+                                
                             </div>
                         ))}
+                    
                 </div>
             </div>
             {loader ? (
@@ -90,8 +122,8 @@ const Label = () => {
                     <Loader size="xl" />
                 </div>
             ) : (
-                <div style={{ display: "grid", gridTemplateColumns: "repeat(3, 1fr)", justifyItems: "center" }}>
-                    {disp && data && data.map((item, index) => {
+                <div className='main-outer-box'>
+                    {display && data && data.map((item, index) => {
                         const sellPrice = (item.slabPricing.length && item.slabPricing[0][2]) || item.itemSellingPricePerUnit;
                         const hasSlab = item.slabPricing.length > 1;
 
@@ -101,13 +133,13 @@ const Label = () => {
                         item.itemMRPperUnit === sellPrice && hasSlab].indexOf(true);
 
                         const fontStyles = ['Line-through-text-size', 'Line-through-size', 'Font6rem', 'Font8rem'];
-                        
+
                         return (
-                            <div key={index} style={{ height: "auto", padding: "5px 0", width: "520px", border: '1px solid black', margin: '5px 0' }}>
-                                <h4 style={{ textAlign: "center", marginBottom: "5px" }}>{item.itemName.slice(0, 30)}</h4>
+                            <div key={index} className='main-inner-box'>
+                                <h4>{item.itemName.slice(0, 30)}</h4>
                                 <hr />
                                 <div className='outer-div'>
-                                    <div style={{ width: `${hasSlab ? '50%' : '100%'}`, display: `${hasSlab ? 'block' : 'flex'}`, justifyContent: `${!hasSlab ? 'space-around' : "center"}` }}>
+                                    <div style={{ width: `${hasSlab ? '50%' : '100%'}`, display: `${hasSlab ? 'block' : 'flex'}`, justifyContent: `${!hasSlab ? 'space-around' : "center"}`, marginTop: "0.5rem" }}>
                                         <div>
                                             <h6 >MRP</h6>
                                             <h4 className={`${fontStyles[truthyIndex]}`}>{item.itemMRPperUnit}</h4>
@@ -144,7 +176,9 @@ const Label = () => {
                             </div>
                         )
                     })}
-                    {!disp && newData && newData.map((item, index) => {
+                    {/* mynewData */}
+                    {!display && newData && newData.map((item, index) => {
+                        console.log(index);
                         const sellPrice = (item.slabPricing.length && item.slabPricing[0][2]) || item.itemSellingPricePerUnit;
                         const hasSlab = item.slabPricing.length > 1;
 
@@ -154,46 +188,51 @@ const Label = () => {
                         item.itemMRPperUnit === sellPrice && hasSlab].indexOf(true);
 
                         const fontStyles = ['Line-through-text-size', 'Line-through-size', 'Font6rem', 'Font8rem'];
-                        
+
                         return (
-                            <div key={index} style={{ height: "auto", padding: "5px 0", width: String(item.itemMRPperUnit).length > 3 ? "520px" : String(item.itemMRPperUnit).length > 2 ? "430px" : "340px", border: '1px solid black', margin: '5px 0' }} id='myLabel'>
-                                <h4 style={{ textAlign: "center", marginBottom: "5px" }}>{item.itemName.slice(0, 30)}</h4>
-                                <hr />
-                                <div style={{ display:"flex",flexDirection:checked?"column":"",alignItems:checked?"center":"",justifyContent:checked?"center":""}}>
-                                    <div style={{ width: `${hasSlab ? '50%' : '100%'}`, display: `${hasSlab ? 'block' : 'flex'}`, justifyContent: `${!hasSlab ? 'space-around' : "center"}`,flexDirection:checked?"column":"" }}>
-                                        <div>
-                                            <h6 >MRP</h6>
-                                            <h4 className={`${fontStyles[truthyIndex]}`}>{item.itemMRPperUnit}</h4>
+                            <div className='show-search-item'>
+                                <div key={index} style={{ height: checked ? "auto" : "360px", padding: "5px 0", width: String(item.itemMRPperUnit).length > 3 ? "520px" : String(item.itemMRPperUnit).length > 2 ? "430px" : "340px", border: '1px solid black', margin: '5px 0', marginTop: "2rem" }} id='myLabel'>
+                                    <h4 style={{ textAlign: "center", width: checked ? '60%' : '', margin: checked ? "0 auto" : "", marginTop: checked ? "0.2rem" : "", marginBottom: checked ? "0.3rem" : "5px" }}>
+                                        {item.itemName.slice(0, 30)}
+                                    </h4>
+                                    <hr />
+                                    <div style={{ display: "flex", flexDirection: checked ? "column" : "", alignItems: checked ? "center" : "", justifyContent: checked ? "center" : "" }}>
+                                        <div style={{ width: `${hasSlab ? '50%' : '100%'}`, display: `${hasSlab ? 'block' : 'flex'}`, justifyContent: `${!hasSlab ? 'space-around' : "center"}`, flexDirection: checked ? "column" : "" }}>
+                                            <div>
+                                                <h6 >MRP</h6>
+                                                <h4 className={`${fontStyles[truthyIndex]}`} >{item.itemMRPperUnit}</h4>
+                                            </div>
+                                            {item.itemMRPperUnit > sellPrice && <div>
+                                                <h6 >Our Price</h6>
+                                                <h2 className={` ${hasSlab ? 'Font6rem' : 'FontSize110px'}`}>{Number(sellPrice).toFixed(0)}</h2>
+                                            </div>}
                                         </div>
-                                        {item.itemMRPperUnit > sellPrice && <div>
-                                            <h6 >Our Price</h6>
-                                            <h2 className={` ${hasSlab ? 'Font6rem' : 'FontSize110px'}`}>{Number(sellPrice).toFixed(0)}</h2>
-                                        </div>}
-                                    </div>
-                                    {hasSlab && (
-                                        <div className='table-settings' style={{ width: '50%' ,display:checked?"flex":"",justifyContent:checked?"center":""}}>
-                                            <table className='tableLayout' style={{position:checked?"relative":"",left:checked?"0.5rem":""}}>
-                                                <thead>
-                                                    <tr>
-                                                        <th >Range</th>
-                                                        <th className='price-align' >Price</th>
-                                                    </tr>
-                                                </thead>
-                                                <tbody>
-                                                    {item.slabPricing.map((slabs, idx, arr) => {
-                                                        return (
-                                                            <tr key={idx}>
-                                                                <td ><span>{`
+                                        {hasSlab && (
+                                            <div className='table-settings' style={{ width: '50%', display: checked ? "flex" : "", justifyContent: checked ? "center" : "" }}>
+                                                <table className='tableLayout' style={{ position: checked ? "relative" : "", left: checked ? "0.5rem" : "" }}>
+                                                    <thead>
+                                                        <tr>
+                                                            <th >Range</th>
+                                                            <th className='price-align' >Price</th>
+                                                        </tr>
+                                                    </thead>
+                                                    <tbody>
+                                                        {item.slabPricing.map((slabs, idx, arr) => {
+                                                            return (
+                                                                <tr key={idx}>
+                                                                    <td ><span>{`
                                     ${slabs[1]} - ${arr[idx + 1] ? arr[idx + 1][1] - 1 : 'more'}`}</span></td>
-                                                                <td className='price-align'>{slabs[2]}</td>
-                                                            </tr>
-                                                        )
-                                                    })}
-                                                </tbody>
-                                            </table>
-                                        </div>
-                                    )}
+                                                                    <td className='price-align'>{slabs[2]}</td>
+                                                                </tr>
+                                                            )
+                                                        })}
+                                                    </tbody>
+                                                </table>
+                                            </div>
+                                        )}
+                                    </div>
                                 </div>
+                                <CloseButton aria-label="Close modal" onClick={()=>handleRemoveItem(index)} className='close-btn'/>
                             </div>
                         )
                     })}
