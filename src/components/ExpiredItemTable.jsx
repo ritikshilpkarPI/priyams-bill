351d9d0233b97478698136ca6096aa6840541b98
dharmendrasahu1999 <dharmendrasahu1999@gmail.com>
2023-01-23 19:06:56 +0530

    to show expired items which are going to expired in according to days

--- src/components/ExpiredItemTable.jsx
+++ src/components/ExpiredItemTable.jsx
@@ -1,213 +1,23 @@
 import { Button, Loader, Table, Card, Title } from "@mantine/core";
-// import { Grid } from '@mantine/core';
 import { DatePicker } from '@mantine/dates';
-// import React, { useState, useEffect } from "react";
 import { Axios } from "src/utils/axios";
-// function ExpiredItemTable({ header }) {
-//     console.log(header)
-//     const [expiredItems, setExpiredItems] = useState([]);
-//     const [loader, setLoader] = useState(false);
-//     const [startDate, setStartDate] = useState("11/16/2022");
-//     const [endDate, setEndDate] = useState("1/7/2023");
-//     const [render, setRender] = useState(false);
-//     const [startDateRange, setStartDateRange] = useState('');
-//     const [endDateRange, setEndDateRange] = useState('');
-//     const SearchDateFilter = () => {
-//         let s = (String(new Date(startDateRange).toLocaleDateString("en-US")));
-//         let e = (String(new Date(endDateRange).toLocaleDateString("en-US")));
-//         setRender(!render);
-//         setStartDate(s);
-//         setEndDate(e);
-//     }
-
-//     const myApiCall = async () => {
-//         setLoader(true);
-//         console.log(1, {
-//             startDate: startDate,
-//             endDate: endDate,
-//         });
-//         const { data } = await Axios.request({
-//             url: "/api/inventory/filterExpiryDates",
-//             method: "POST",
-//             data: {
-//                 startDate: startDate,
-//                 endDate: endDate,
-//             },
-//             headers: {
-//                 Cookie: "",
-//             },
-//         });
-
-//         data.message.sort(function (a, b) {
-//             let dateA = new Date(a.useByDate[0].date);
-//             let dateB = new Date(b.useByDate[0].date);
-//             return dateA - dateB;
-//         });
-//         setExpiredItems(data.message);
-//         setLoader(false);
-//     }
-//     const handlingDate = (day, start_Date, end_Date) => {
-//         console.log({day, start_Date, end_Date});
-//         end_Date = start_Date.setDate(start_Date.getDate() + day);
-//         end_Date = String(new Date(end_Date).toLocaleDateString());
-//         start_Date = new Date();
-//         start_Date = String(new Date(start_Date).toLocaleDateString());
-
-//         console.log({day, start_Date, end_Date});
-//         return [day, start_Date, end_Date]
-//     }
-//     const getExpiredData = async () => {
-//         try {
-//             let start_Date = new Date();
-//             let day = 0;
-//             let end_Date
-
-//             if (header === "1 day") {
-
-//                 day = Number(1);
-//                 [day, start_Date, end_Date] = handlingDate(day, start_Date, end_Date)
-//             }
-//             else if (header === "7 days") {
-//                 day = Number(7);
-//                 [day, start_Date, end_Date] = handlingDate(day, start_Date, end_Date)
-//             }
-//             else if (header === "15 days") {
-//                 day = Number(15);
-//                 [day, start_Date, end_Date] = handlingDate(day, start_Date, end_Date)
-//             }
-//             else if (header === "30 days") {
-//                 day = Number(30);
-//                 [day, start_Date, end_Date] = handlingDate(day, start_Date, end_Date)
-//             }
-//             setLoader(true);
-//             console.log(2, {
-//                 startDate: start_Date,
-//                 endDate: end_Date,
-//             });
-//             const { data } = await Axios.request({
-//                 url: "/api/inventory/filterExpiryDates",
-//                 method: "POST",
-//                 data: {
-//                     startDate: start_Date,
-//                     endDate: end_Date,
-//                 },
-//                 headers: {
-//                     Cookie: "",
-//                 },
-//             });
-
-//             data.message.sort(function (a, b) {
-//                 let dateA = new Date(a.useByDate[0].date);
-//                 let dateB = new Date(b.useByDate[0].date);
-//                 return dateA - dateB;
-//             });
-//             setExpiredItems(data.message);
-//             setLoader(false);
-//         } catch (err) {
-//             console.log({ err });
-//         }
-//     };
-//     useEffect(() => {
-//         myApiCall();
-//         // eslint-disable-next-line
-//     }, [render])
-
-//     useEffect(() => {
-//         const timer = setTimeout(() => {
-//             getExpiredData();
-//         }, 1000);
-//         return () => clearTimeout(timer);
-//         // eslint-disable-next-line
-//     }, [])
-
-//     return (
-//         <div className="outer-div" >
-//             {loader ? (
-//                 <div
-//                     style={{
-//                         height: "95vh",
-//                         width: "100%",
-//                         display: "flex",
-//                         justifyContent: "center",
-//                         alignItems: "center",
-//                     }}
-//                 >
-//                     <Loader size="xl" />
-//                 </div>
-//             ) :
-//                 (<Card shadow="sm" p="lg" radius="md" withBorder className=" toggle-height">
-
-//                     <Card.Section>
-//                         <Title order={2} className="position-heading">{header}</Title>
-//                         {(header === "30 days") ?
-//                             (
-//                                 <Grid className="date-margin">
-//                                     <Grid.Col span={4}>
-//                                         <DatePicker placeholder="Start date" value={startDateRange} withAsterisk onChange={setStartDateRange} />
-//                                     </Grid.Col>
-//                                     <Grid.Col span={4}>
-//                                         <DatePicker placeholder="End date" value={endDateRange} withAsterisk onChange={setEndDateRange} />
-//                                     </Grid.Col>
-//                                     <Grid.Col span={4}>
-//                                         <Button className="search-filter-btn" onClick={() => SearchDateFilter()} >Search Date Filter</Button>
-//                                     </Grid.Col>
-//                                 </Grid>
-//                             )
-//                             :
-//                             ""
-//                         }
-//                     </Card.Section>
-//                     <Card.Section className="table-scroll">
-//                         <Table striped highlightOnHover withBorder withColumnBorders >
-//                             <thead>
-//                                 <th>Barcode</th>
-//                                 <th>Name</th>
-//                                 <th>Expiry</th>
-//                                 <th>Total Qty</th>
-//                             </thead>
-//                             <tbody>
-//                                 {expiredItems &&
-//                                     expiredItems.map((item, index) => {
-//                                         return (
-//                                             <tr key={index}>
-//                                                 <td>{item.itemBarcode}</td>
-//                                                 <td>{item.itemName}</td>
-//                                                 <Table withBorder withColumnBorders>
-//                                                     <tbody>
-//                                                         {item.useByDate.map((expiry,index) => {
-//                                                             return (
-//                                                                 <tr key={index}>
-//                                                                     <td style={{ width: "40%" }}>{`${new Date(
-//                                                                         expiry.date
-//                                                                     ).getDay()}/${new Date(expiry.date).getMonth() + 1
-//                                                                         }/${new Date(expiry.date).getFullYear()}`}</td>
-//                                                                     <td>{expiry.value}</td>
-//                                                                 </tr>
-//                                                             );
-//                                                         })}
-//                                                     </tbody>
-//                                                 </Table>
-//                                                 <td>{item.totalItems}</td>
-//                                             </tr>
-//                                         );
-//                                     })}
-//                             </tbody>
-//                         </Table>
-//                     </Card.Section>
-//                 </Card>
-//                 )}
-//         </div>
-//     )
-// }
-
-// export default ExpiredItemTable
-
 import React, { useState, useEffect } from 'react';
 
 const addDays = (date, days) => {
-    var result = new Date(date);
-    result.setDate(result.getDate() + days);
-    return result;
+
+   let dayToIncr={
+        1:0,
+        7:2,
+        15:8,
+        30:16
+    }
+   
+    let currentDate = new Date(date);
+    currentDate.setDate(currentDate.getDate()+dayToIncr[days]);
+    let endDate = new Date(date);
+    endDate.setDate(endDate.getDate()+days );
+    return {currentDate,endDate}
+
 };
 
 const getExpiredApiData = async (startDate, endDate) => {
@@ -227,105 +37,84 @@ const ExpiredItemTable = ({ day }) => {
     const [startDateValue, setStartDateValue] = useState(new Date());
     const [endDateValue, setEndDateValue] = useState(addDays(new Date(), 30));
     const [data, setData] = useState([]);
+    const [loader, setLoader] = useState(false);
 
     useEffect(() => {
         const getExpiredData = async () => {
-            const endDate = addDays(startDateValue, day);
-
-            const response = await getExpiredApiData(startDateValue, endDate);
-            setData(response.message);
+            const {currentDate,endDate} = addDays(startDateValue, day);
+            setLoader(true);
+            const response = await getExpiredApiData(currentDate, endDate);
+            setData(response.message.expiredItems);
+            setLoader(false);
         }
+        // eslint-disable-next-line
         getExpiredData();
+        // eslint-disable-next-line
     }, []);
 
     const handleDateSearch = async () => {
+        setLoader(true);
         const response = await getExpiredApiData(startDateValue, endDateValue);
-        setData(response.message);
+        setData(response.message.expiredItems);
+        setLoader(false);
     }
-
+    console.log(day, data);
     return (
-        <>
-            <h1>{day} Days</h1>
-            {day === 30 && <DatePicker placeholder="Start date" value={startDateValue} withAsterisk onChange={setStartDateValue} />}
-            {day === 30 && <DatePicker placeholder="Start date" value={endDateValue} withAsterisk onChange={setEndDateValue} />}
-            {day === 30 && <Button onClick={handleDateSearch}>Search</Button>}
-        </>
-        // <div className="outer-div" >
-        //     {loader ? (
-        //         <div
-        //             style={{
-        //                 height: "95vh",
-        //                 width: "100%",
-        //                 display: "flex",
-        //                 justifyContent: "center",
-        //                 alignItems: "center",
-        //             }}
-        //         >
-        //             <Loader size="md" />
-        //         </div>
-        //     ) :
-        //         (<Card shadow="sm" p="lg" radius="md" withBorder className=" toggle-height">
+        <div className="outer-div" >
+            {loader ? (
+                <div
+                    style={{
+                        height: "95vh",
+                        width: "100%",
+                        display: "flex",
+                        justifyContent: "center",
+                        alignItems: "center",
+                    }}
+                >
+                    <Loader size="md" />
+                </div>
+            ) :
+                (<Card shadow="sm" p="lg" radius="md" withBorder className=" toggle-height">
 
-        //             <Card.Section>
-        //                 <Title order={2} className="position-heading">{header}</Title>
-        //                 {(header === "30 days") ?
-        //                     (
-        //                         <Grid className="date-margin">
-        //                             <Grid.Col span={4}>
-        //                                 <DatePicker placeholder="Start date" value={startDateRange} withAsterisk onChange={setStartDateRange} />
-        //                             </Grid.Col>
-        //                             <Grid.Col span={4}>
-        //                                 <DatePicker placeholder="End date" value={endDateRange} withAsterisk onChange={setEndDateRange} />
-        //                             </Grid.Col>
-        //                             <Grid.Col span={4}>
-        //                                 <Button className="search-filter-btn" onClick={() => SearchDateFilter()} >Search Date Filter</Button>
-        //                             </Grid.Col>
-        //                         </Grid>
-        //                     )
-        //                     :
-        //                     ""
-        //                 }
-        //             </Card.Section>
-        //             <Card.Section className="table-scroll">
-        //                 <Table striped highlightOnHover withBorder withColumnBorders >
-        //                     <thead>
-        //                         <th>Barcode</th>
-        //                         <th>Name</th>
-        //                         <th>Expiry</th>
-        //                         <th>Total Qty</th>
-        //                     </thead>
-        //                     <tbody>
-        //                         {expiredItems &&
-        //                             expiredItems.map((item, index) => {
-        //                                 return (
-        //                                     <tr key={index}>
-        //                                         <td>{item.itemBarcode}</td>
-        //                                         <td>{item.itemName}</td>
-        //                                         <Table withBorder withColumnBorders>
-        //                                             <tbody>
-        //                                                 {item.useByDate.map((expiry, index) => {
-        //                                                     return (
-        //                                                         <tr key={index}>
-        //                                                             <td style={{ width: "40%" }}>{`${new Date(
-        //                                                                 expiry.date
-        //                                                             ).getDay()}/${new Date(expiry.date).getMonth() + 1
-        //                                                                 }/${new Date(expiry.date).getFullYear()}`}</td>
-        //                                                             <td>{expiry.value}</td>
-        //                                                         </tr>
-        //                                                     );
-        //                                                 })}
-        //                                             </tbody>
-        //                                         </Table>
-        //                                         <td>{item.totalItems}</td>
-        //                                     </tr>
-        //                                 );
-        //                             })}
-        //                     </tbody>
-        //                 </Table>
-        //             </Card.Section>
-        //         </Card>
-        //         )}
-        // </div>
+                    <Card.Section>
+                        <Title order={2} className="position-heading">{day} days</Title>
+                    </Card.Section>
+                    <Card.Section>
+                        <div className="parent-datePicker">
+                            {day === 30 && <DatePicker placeholder="Start date" value={startDateValue} withAsterisk onChange={setStartDateValue} />}
+                            {day === 30 && <DatePicker placeholder="End date" value={endDateValue} withAsterisk onChange={setEndDateValue} />}
+                            {day === 30 && <Button onClick={handleDateSearch}>Search</Button>}
+                        </div>
+
+                    </Card.Section>
+                    <Card.Section className="table-scroll">
+                        <Table striped highlightOnHover withBorder withColumnBorders >
+                            <thead>
+                                <th>Barcode</th>
+                                <th>Name</th>
+                                <th>Expiry</th>
+                                <th>Total Qty</th>
+                            </thead>
+                            <tbody>
+                                {data &&
+                                    data.map((item, index) => {
+                                        return (
+                                            <tr key={index}>
+                                                <td>{item.itemBarcode}</td>
+                                                <td>{item.itemName}</td>
+                                                <td>{new Date(item.useByDate.date).toLocaleDateString()}</td>
+                                                <td>{item.useByDate.quantity}</td>
+                                            </tr>
+                                        );
+                                    })}
+                            </tbody>
+                        </Table>
+                    </Card.Section>
+                </Card>
+                )}
+        </div>
+        // </>
+
     )
 }
 
