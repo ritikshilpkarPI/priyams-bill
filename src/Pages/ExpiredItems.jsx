e764e794d8f35c4fa625a92a1557cb16a8723338
Yash priyam <yashpriyam74@gmail.com>
2023-01-28 18:59:05 +0530

    remove unused fragment

--- src/Pages/ExpiredItems.jsx
+++ src/Pages/ExpiredItems.jsx
@@ -1,20 +1,18 @@
-import { Grid } from '@mantine/core';
+import { Grid } from "@mantine/core";
 import React from "react";
 import ExpiredItemTable from "src/components/ExpiredItemTable";
 
 const ExpiredItems = () => {
-  const dateIntervalArr = [1, 7, 15, 30]
-  
+  const dateIntervalArr = [1, 7, 15, 30];
+
   return (
-    <>
-      <Grid>
-        {dateIntervalArr.map((dayInterval) => (
-          <Grid.Col sm={6} lg={6}>
-            <ExpiredItemTable day={dayInterval} />
-          </Grid.Col>
-        ))}
-      </Grid>
-    </>
+    <Grid>
+      {dateIntervalArr.map((dayInterval) => (
+        <Grid.Col sm={6} lg={6}>
+          <ExpiredItemTable day={dayInterval} />
+        </Grid.Col>
+      ))}
+    </Grid>
   );
 };
 
