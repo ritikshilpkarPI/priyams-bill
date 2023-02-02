f0dd9d21bbbbc64bafd584e1584b6911c18283d3
dharmendrasahu1999 <dharmendrasahu1999@gmail.com>
2023-02-02 12:22:24 +0530

    search functionality

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,56 +1,107 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { sidebarListData } from '../constants/HeaderTypes';
 import { Link } from 'react-router-dom';
 
+const handleToggle = (id) => {
+  let toggledElement = document.getElementById(`linkContainer${id}`);
+
+  // Closing and opening links on title click
+  toggledElement.style.display = toggledElement.style.display === "none" ? "block" : "none";
+}
+function searchSidebarListData(searchTerm) {
+  const results = [];
+
+  sidebarListData.forEach(item => {
+    const subItems = [];
+    Object.values(item)[0].forEach(subItem => {
+      if (subItem.name.toLowerCase().includes(searchTerm.toLowerCase())) {
+        subItems.push(subItem);
+      }
+    });
+    if (subItems.length > 0) {
+      results.push({ [Object.keys(item)[0]]: subItems });
+    }
+  });
+
+  return results;
+}
 function Header() {
-   const handleToggle = (id) => {
-      let toggledElement = document.getElementById(`linkContainer${id}`);
 
-      // Closing and opening links on title click
-      toggledElement.style.display = toggledElement.style.display === "none" ? "block" : "none";
-   }
+  const [value, setValue] = useState('')
+  const [filterData, setFilterData] = useState([]);
+  const setInputValue = (e) => {
+    setValue(e.target.value);
+  }
+  let searchSidebarFilterListData
+  useEffect(() => {
+    searchSidebarFilterListData = searchSidebarListData(value);
+    setFilterData(searchSidebarFilterListData);
+    console.log('chal rha he')
+    //   searchSidebarFilterListData.forEach(item => {
+    //     console.log(Object.keys(item)[0]);
+    //     console.log(Object.values(item)[0]);
+    // })
+  }, [value])
 
-   return (
-      <div id='main-box-container'>
-         <div className='profileName'>
-            <img src="images/hamburger.svg" width={25} height={25} className='hamburger-menu' />
-            <h3 className='user-name'>Sachin Rawat</h3>
-         </div>
+  return (
+    <div id='main-box-container'>
+      <div className='profileName'>
+        <img src="images/hamburger.svg" width={25} height={25} className='hamburger-menu' alt="" />
+        <h3 className='user-name'>Sachin Rawat</h3>
+      </div>
 
-         {/* Search Input */}
-         <input type="text" name="search" id="search-input" placeholder='Search' />
+      {/* Search Input */}
+      <input type="text" name="search" id="search-input" placeholder='Search' value={value} onChange={(e) => setInputValue(e)} />
 
-         {/* Billing Button */}
-         <button className="billing">Billing</button>
+      {/* Billing Button */}
+      <button className="billing">Billing</button>
 
-         <nav className="sidebar">
-            {sidebarListData.map((item, index) => {
-               const [itemKey, itemValue] = Object.entries(item)[0];
-               return (
-                  <div key={index} className="links-container">
-                     {/* Main Title */}
-                     <p className="main-link headers" onClick={() => handleToggle(index)}>{itemKey}</p>
+      <nav className="sidebar">
+        {
+          value === '' ? (sidebarListData.map((item, index) => {
+            const [itemKey, itemValue] = Object.entries(item)[0];
+            return (
+              <div key={index} className="links-container">
+                {/* Main Title */}
+                <p className="main-link headers" onClick={() => handleToggle(index)}>{itemKey}</p>
 
-                     {/* Link Items */}
-                     <ul id={`linkContainer${index}`}>
-                        {itemValue.map((subItem, index) => {
-                           return (
-                              <li key={index}>
-                                 <img src={`${subItem.icon}`} width={20} height={20} title={`${subItem.name}`} />
-                                 <Link to={`${subItem.url}`} className='headers-list'>{subItem.name}</Link>
-                              </li>
-                           )
-                        })}
-                     </ul>
-                  </div>
-               )
-            })}
-         </nav>
+                {/* Link Items */}
+                <ul id={`linkContainer${index}`}>
+                  {itemValue.map((subItem, index) => {
+                    return (
+                      <li key={index}>
+                        <img src={`${subItem.icon}`} width={20} height={20} title={`${subItem.name}`} alt="" />
+                        <Link to={`${subItem.url}`} className='headers-list'>{subItem.name}</Link>
+                      </li>
+                    )
+                  })}
+                </ul>
+              </div>
+            )
+          })) : (
 
-         {/* Logout Button */}
-         <button className="logout">Logout</button>
-      </div >
-   )
+            filterData.forEach(item => {
+              console.log('chal rha')
+              console.log(Object.keys(item)[0]);
+              Object.values(item)[0].map((subItem, index) => {
+                return (
+                  <ul>
+                    <li key={index}>
+                      <img src={`${subItem.icon}`} width={20} height={20} title={`${subItem.name}`} alt="" />
+                      <Link to={`${subItem.url}`} className='headers-list'>{subItem.name}</Link>
+                    </li>
+                  </ul>
+                )
+              })
+            })
+          )
+        }
+      </nav>
+
+      {/* Logout Button */}
+      <button className="logout">Logout</button>
+    </div >
+  )
 }
 
 export default Header
--- src/constants/HeaderTypes.js
+++ src/constants/HeaderTypes.js
@@ -16,26 +16,26 @@ export const PAGES = {
 
 export const sidebarListData = [
     {
-       ITEM: [{ name: 'Inventory', url: '/label', icon: 'images/inventory.svg' }, { name: 'Shortage Items', url: '/label', icon: 'images/shortage.svg' },
-       { name: 'Expired Items', url: '/label', icon: 'images/expired.svg' }, { name: 'Item Labels', url: '/label', icon: 'images/label.svg' }
+       ITEM: [{ name: 'Inventory', url: '/inventory', icon: 'images/inventory.svg' }, { name: 'Shortage Items', url: '/label', icon: 'images/shortage.svg' },
+       { name: 'Expired Items', url: '/expiredItems', icon: 'images/expired.svg' }, { name: 'Item Labels', url: '/label', icon: 'images/label.svg' }
        ]
     },
     {
-       BILL: [{ name: 'All Bills', url: '/label', icon: 'images/bill.svg' }, { name: 'Day Bills', url: '/label', icon: 'images/dailybills.svg' },
-       { name: 'Reports', url: '/label', icon: 'images/report.svg' }
+       BILL: [{ name: 'All Bills', url: '/allBill', icon: 'images/bill.svg' }, { name: 'Day Bills', url: '/dayBill', icon: 'images/dailybills.svg' },
+       { name: 'Reports', url: '/report', icon: 'images/report.svg' }
        ]
     },
     {
-       STAFF: [{ name: 'Open Close', url: '/label', icon: 'images/openclose.svg' },
-       { name: 'Attendance', url: '/label', icon: 'images/attendance.svg' }, { name: 'Profile (New)', url: '/label', icon: 'images/profile.svg' }
+       STAFF: [{ name: 'Open Close', url: '/openClose', icon: 'images/openclose.svg' },
+       { name: 'Attendance', url: '/attendance', icon: 'images/attendance.svg' }, { name: 'Profile (New)', url: '/label', icon: 'images/profile.svg' }
        ]
     },
     {
        PURCHASE: [
-          { name: 'Purchase Order', url: '/label', icon: 'images/purchaseorder.svg' },
+          { name: 'Purchase Order', url: '/purchase', icon: 'images/purchaseorder.svg' },
           { name: 'Saved POs', url: '/label', icon: 'images/savedpurchaseorder.svg' },
           { name: 'Drafted POs', url: '/label', icon: 'images/draftpurchaseorder.svg' },
-          { name: 'Approved POs', url: '/label', icon: 'images/approvedpurchaseorder.svg' },
+          { name: 'Approved POs', url: '/approval', icon: 'images/approvedpurchaseorder.svg' },
           { name: 'Rejected POs', url: '/label', icon: 'images/rejectpurchaseorder.svg' }
        ]
     }
