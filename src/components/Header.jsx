264429985d6ffb5d01b5be2e1776e62427d148da
dharmendrasahu1999 <dharmendrasahu1999@gmail.com>
2023-02-02 19:19:58 +0530

    sidebar toggling done

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,85 +1,135 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import { sidebarListData } from '../constants/HeaderTypes';
-import { Link } from 'react-router-dom';
+import { Link , useHistory} from 'react-router-dom';
 
 const handleToggle = (id) => {
-   let toggledElement = document.getElementById(`linkContainer${id}`);
+  let toggledElement = document.getElementById(`linkContainer${id}`);
 
-   // Closing and opening links on title click
-   toggledElement.style.display = toggledElement.style.display === "none" ? "block" : "none";
+  // Closing and opening links on title click
+  toggledElement.style.display = toggledElement.style.display === "none" ? "block" : "none";
 }
 
 function searchSidebarListData(searchTerm) {
-   const results = [];
+  const results = [];
 
-   sidebarListData.forEach(item => {
-      const subItems = [];
-      Object.values(item)[0].forEach(subItem => {
-         if (subItem.name.toLowerCase().includes(searchTerm.toLowerCase())) {
-            subItems.push(subItem);
-         }
-      });
-      if (subItems.length > 0) {
-         results.push({ [Object.keys(item)[0]]: subItems });
+  sidebarListData.forEach(item => {
+    const subItems = [];
+    Object.values(item)[0].forEach(subItem => {
+      if (subItem.name.toLowerCase().includes(searchTerm.toLowerCase())) {
+        subItems.push(subItem);
       }
-   });
+    });
+    if (subItems.length > 0) {
+      results.push({ [Object.keys(item)[0]]: subItems });
+    }
+  });
 
-   return results;
+  return results;
 }
 
 const Header = () => {
-   const [value, setValue] = useState('')
-   const [listItemsData, setListItemsData] = useState(sidebarListData);
-   const setInputValue = (e) => {
-      setValue(e.target.value);
-   } 
-   useEffect(() => {
-      setListItemsData(searchSidebarListData(value));
-   }, [value])
+  const [value, setValue] = useState('')
+  const [listItemsData, setListItemsData] = useState(sidebarListData);
 
-   return (
-      <div id='main-box-container'>
-         <div className='profileName'>
-            <img src="images/hamburger.svg" width={25} height={25} className='hamburger-menu' alt="" />
-            <h3 className='user-name'>Sachin Rawat</h3>
-         </div>
+  const history = useHistory()
 
-         {/* Search Input */}
-         <input type="text" name="search" id="search-input" placeholder='Search' value={value} onChange={(e) => setInputValue(e)} />
+  const inputElem = useRef('')
+  const liItem = useRef([])
+  const profile=useRef('')
+  const mainCont=useRef('');
+  const setInputValue = (e) => {
+    setValue(e.target.value);
+  }
 
-         {/* Billing Button */}
-         <button className="billing">Billing</button>
+  useEffect(() => {
+    setListItemsData(searchSidebarListData(value));
+  }, [value])
+  
 
-         <nav className="sidebar">
-            {
-               listItemsData.map((item, index) => {
-                  const [itemKey, itemValue] = Object.entries(item)[0];
-                  return (
-                     <div key={index} className="links-container">
-                        {/* Main Title */}
-                        <p className="main-link headers" onClick={() => handleToggle(index)}>{itemKey}</p>
+  const goToBilling=()=>{
+    history.push('/billing');
+  }
 
-                        {/* Link Items */}
-                        <ul id={`linkContainer${index}`}>
-                           {itemValue.map((subItem, index) => {
-                              return (
-                                 <li key={index}>
-                                    <img src={`${subItem.icon}`} width={20} height={20} title={`${subItem.name}`} alt="" />
-                                    <Link to={`${subItem.url}`} className='headers-list'>{subItem.name}</Link>
-                                 </li>
-                              )
-                           })}
-                        </ul>
-                     </div>
-                  )
-               })
-            }
-         </nav>
+  const handleToggleOuterView = () => {
+    console.log(mainCont.current.children)
+    if (inputElem.current.style.display === "none") {
+      inputElem.current.style.display = "block";
+      profile.current.children[1].style.display="block";
+      profile.current.children[2].style.display="none";
+      mainCont.current.style.width="300px";
+      mainCont.current.children[2].innerText="Billing";
+      mainCont.current.children[4].innerText="Logout";
 
-         {/* Logout Button */}
-         <button className="logout">Logout</button>
-      </div >
-   )
+
+      liItem.current.forEach(ele => {
+        ele.children[0].style.display="block";
+        ele.children[1].childNodes.forEach(element => {
+          element.children[0].children[1].style.display="block";
+
+        })
+      })
+    }
+    else {
+      inputElem.current.style.display = "none"
+      profile.current.children[1].style.display="none";
+      profile.current.children[2].style.display="block";
+      mainCont.current.style.width="100px";
+      mainCont.current.children[2].innerText="B";
+      mainCont.current.children[4].innerText="L";
+
+      liItem.current.forEach(ele => {
+        ele.children[0].style.display="none";
+        ele.children[1].childNodes.forEach(element => {
+          element.children[0].children[1].style.display="none";
+        })
+      })
+    }
+  }
+  return (
+    <div id='main-box-container' ref={mainCont}>
+      <div className='profileName' ref={profile}>
+        <img src="images/hamburger.svg" width={25} height={25} className='hamburger-menu' alt="" onClick={handleToggleOuterView} />
+        <h3 className='user-name' >Sachin Rawat</h3>
+        <div className='profile-avatar'>S</div>
+      </div>
+
+      {/* Search Input */}
+      <input type="text" ref={inputElem} name="search" id="search-input" placeholder='Search' value={value} onChange={(e) => setInputValue(e)} />
+
+      {/* Billing Button */}
+      <button className="billing" onClick={goToBilling}>Billing</button>
+      <nav className="sidebar">
+        {
+          listItemsData.map((item, index) => {
+            const [itemKey, itemValue] = Object.entries(item)[0];
+            return (
+              <div key={index} className="links-container" ref={(el) => (liItem.current[index] = el)}>
+                {/* Main Title */}
+                <p className="main-link headers" onClick={() => handleToggle(index)}>{itemKey}</p>
+
+                {/* Link Items */}
+                <ul id={`linkContainer${index}`}>
+                  {itemValue.map((subItem, index) => {
+                    return (
+                      <li key={index} >
+                        <Link to={`${subItem.url}`} >
+                        <img src={`${subItem.icon}`} width={30} height={30} title={`${subItem.name}`} alt={`${subItem.name}`} />
+                        <p className='headers-list'>{subItem.name}</p>
+                        </Link>
+                      </li>
+                    )
+                  })}
+                </ul>
+              </div>
+            )
+          })
+        }
+      </nav>
+
+      {/* Logout Button */}
+      <button className="logout">Logout</button>
+    </div >
+  )
 }
 
 export default Header
--- src/constants/HeaderTypes.js
+++ src/constants/HeaderTypes.js
@@ -16,7 +16,7 @@ export const PAGES = {
 
 export const sidebarListData = [
     {
-       ITEM: [{ name: 'Inventory', url: '/inventory', icon: 'images/inventory.svg' }, { name: 'Shortage Items', url: '/label', icon: 'images/shortage.svg' },
+       ITEM: [{ name: 'Inventory', url: '/inventory', icon: 'images/inventory.svg' }, { name: 'Shortage Items', url: '/stockquantity', icon: 'images/shortage.svg' },
        { name: 'Expired Items', url: '/expiredItems', icon: 'images/expired.svg' }, { name: 'Item Labels', url: '/label', icon: 'images/label.svg' }
        ]
     },
@@ -27,16 +27,16 @@ export const sidebarListData = [
     },
     {
        STAFF: [{ name: 'Open Close', url: '/openClose', icon: 'images/openclose.svg' },
-       { name: 'Attendance', url: '/attendance', icon: 'images/attendance.svg' }, { name: 'Profile (New)', url: '/label', icon: 'images/profile.svg' }
+       { name: 'Attendance', url: '/attendance', icon: 'images/attendance.svg' }, { name: 'Profile (New)', url: '/', icon: 'images/profile.svg' }
        ]
     },
     {
        PURCHASE: [
           { name: 'Purchase Order', url: '/purchase', icon: 'images/purchaseorder.svg' },
-          { name: 'Saved POs', url: '/label', icon: 'images/savedpurchaseorder.svg' },
-          { name: 'Drafted POs', url: '/label', icon: 'images/draftpurchaseorder.svg' },
+          { name: 'Saved POs', url: '/approval', icon: 'images/savedpurchaseorder.svg' },
+          { name: 'Drafted POs', url: '/approval', icon: 'images/draftpurchaseorder.svg' },
           { name: 'Approved POs', url: '/approval', icon: 'images/approvedpurchaseorder.svg' },
-          { name: 'Rejected POs', url: '/label', icon: 'images/rejectpurchaseorder.svg' }
+          { name: 'Rejected POs', url: '/approval', icon: 'images/rejectpurchaseorder.svg' }
        ]
     }
  ];
