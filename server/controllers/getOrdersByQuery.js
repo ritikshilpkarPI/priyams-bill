49db94e772ef58a1265645cb8091b905da311e4e
Priyanshi-Yadav-1304 <priyanshiy66@gmail.com>
2023-03-17 18:51:47 +0530

    remove unneccessory cloudinary config code

--- server/controllers/getOrdersByQuery.js
+++ server/controllers/getOrdersByQuery.js
@@ -1,11 +1,5 @@
 const PurchaseOrder = require('../db-models/purchase-order-model');
-const cloudinary = require('cloudinary');
 
-cloudinary.config({
-  cloud_name: process.env.CLOUD_NAME,
-  api_key: process.env.CLOUD_API_KEY,
-  api_secret: process.env.CLOUD_API_SECRET,
-});
 
 const getOrdersByQuery = async (req, res) => {
     try {
--- server/controllers/updateOrderByIndex.js
+++ server/controllers/updateOrderByIndex.js
@@ -1,11 +1,4 @@
 const PurchaseOrder = require('../db-models/purchase-order-model');
-const cloudinary = require('cloudinary');
-
-cloudinary.config({
-  cloud_name: process.env.CLOUD_NAME,
-  api_key: process.env.CLOUD_API_KEY,
-  api_secret: process.env.CLOUD_API_SECRET,
-});
 
 const updateOrderByIndex = async (req, res) => {
     try {
