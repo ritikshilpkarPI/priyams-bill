ec5d3c97a714e986e101d959399af060f0bb3664
Yash priyam <yashpriyam74@gmail.com>
2023-03-29 14:02:03 +0530

    addd logout user controller

--- server/controllers/loginUser.js
+++ server/controllers/loginUser.js
@@ -24,14 +24,5 @@ const loginUser = async (req, res, next) => {
     success: true,
   });
 };
-const logoutUser = async (req, res, next) => {
-  res.cookie('token', '', {
-    expires: new Date(Date.now()),
-  });
-  res.status(200).json({
-    success: true,
-    message: 'Logout successfully',
-  });
-};
 
 module.exports = loginUser;
