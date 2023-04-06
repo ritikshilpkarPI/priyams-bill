66251e2c00a4b3ac87592134ab7f37b2c712e1ea
Yash priyam <yashpriyam74@gmail.com>
2023-04-06 15:32:52 +0530

    hide product details in customer bill

--- server/controllers/getEditBill.js
+++ server/controllers/getEditBill.js
@@ -1,21 +1,21 @@
 const { Bill } = require('../db-models/bill-model');
 
-const getEditBill = async (req, res) => {
-    try {
-      const id = req.params.id;
-      const bill = await Bill.findById(id).populate({
-        path: 'items',
-        populate: {
-          path: 'itemDetail',
-          model: 'Item',
-        },
-      });
-  
-      res.status(200).json({ message: bill });
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ error: error.message });
-    }
-  };
+const getEditBill = async (req, res, next) => {
+  try {
+    const id = req.params.id;
+    const bill = await Bill.findById(id).populate({
+      path: 'items',
+      populate: {
+        path: 'itemDetail',
+        model: 'Item',
+        select: 'itemBrandName itemCategory itemName',
+      },
+    });
 
-  module.exports = getEditBill;
+    res.status(200).json({ message: bill });
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = getEditBill;
