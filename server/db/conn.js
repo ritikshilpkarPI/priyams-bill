b9ccdf5ecd4c071309329a5d1d09155c4e92024a
Hs809 <hiteshpal.8097@gmail.com>
2023-04-14 19:50:16 +0530

    fix app connnection, retry the connection if close

--- server/db/conn.js
+++ server/db/conn.js
@@ -2,23 +2,47 @@ const { default: mongoose } = require("mongoose");
 const { orderSchema, Order } = require("../db-models/orderSchema");
 
 const dbAppConnection = () => {
-    try {
-      const conn = mongoose.createConnection(process.env.APP_MONGODB_URI, { useNewUrlParser: true });
-      console.log("App Database Connected Successfully!");
-      conn.model("Orders", orderSchema);
-      const pipeline =  [
-          { $match : {"operationType" : "update" } }
-       ]
+  try {
+    const db = mongoose.createConnection(process.env.APP_MONGODB_URI, { useNewUrlParser: true });
+
+    db.on('connecting', function () {
+      console.log('connecting to MongoDB...');
+    });
+
+    db.on('error', function (error) {
+      console.error('Error in MongoDb connection: ' + error);
+      mongoose.disconnect();
+    });
+    db.on('connected', function () {
+      console.log('MongoDB connected!');
+
+    });
+    db.once('open', function () {
+      console.log('MongoDB connection opened!');
+      db.model("Orders", orderSchema);
+      const pipeline = [
+        { $match: { "operationType": "update" } }
+      ]
       const changeStream = Order.watch(pipeline, { fullDocument: "updateLookup" });
-      changeStream.on("change", async(data) => {
+      changeStream.on("change", async (data) => {
         const order = data.fullDocument;
-        const {orderNumber,orderStatus} = order
+        const { orderNumber, orderStatus } = order
         // Update Order in App Database
-        await conn.models.Orders.findOneAndUpdate({orderNumber},{orderStatus}, {new: true});   
+
+        const updateddata = await db.models.Orders.findOneAndUpdate({ orderNumber }, { orderStatus }, { new: true });
+        console.log({ updateddata });
       });
-    } catch (error) {
-      console.error({ error });
-    }
-  };
+    });
+    db.on('reconnected', function () {
+      console.log('MongoDB reconnected!');
+    });
+    db.on('disconnected', function () {
+      console.log('MongoDB disconnected!');
+      mongoose.connect(process.env.APP_MONGODB_URI, { server: { auto_reconnect: true }, useNewUrlParser: true });
+    });
+  } catch (error) {
+    console.error({ error });
+  }
+};
 
 module.exports = dbAppConnection
