dcd4dbf23d8d7ebcef61e564686bf22d8e687e66
Hs809 <hiteshpal.8097@gmail.com>
2023-04-20 00:41:37 +0530

    add images type to update model and update editItemById controller to store image in clodinary and update in db

--- server/controllers/editItemById.js
+++ server/controllers/editItemById.js
@@ -1,15 +1,20 @@
+const { uploadImages } = require('../util/image');
 const { Item } = require('../db-models/item-model');
 
-const editItemById = async (req, res,next) => {
-    try {
-      const { id, itemToBeUpdated } = req.body;
-      const changedItem = await Item.findByIdAndUpdate(id, itemToBeUpdated, {
-        new: true,
-      });
-      res.status(200).json({ message: changedItem });
-    } catch (error) {
-      next(error)
-    }
-  };
+const editItemById = async (req, res, next) => {
+  try {
+    const { id, itemToBeUpdated } = req.body;
+    const imagesToUpload = itemToBeUpdated.images.filter(image => !image.public_id).map(image => image.secure_url)
+    const images = itemToBeUpdated.images.filter(image => image.public_id)
+    let uploadedImages = await uploadImages(imagesToUpload);
+    itemToBeUpdated.images = [...uploadedImages, ...images]
+    const changedItem = await Item.findByIdAndUpdate(id, itemToBeUpdated, {
+      new: true,
+    });
+    res.status(200).json({ message: changedItem });
+  } catch (error) {
+    next(error)
+  }
+};
 
-  module.exports = editItemById;
+module.exports = editItemById;
