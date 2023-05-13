0601ea657034206f653b354f5d7ce6b50ea2ed7d
Yash priyam <yashpriyam74@gmail.com>
2023-05-13 14:20:56 +0530

    add item name and remove costprice to bills

--- server/controllers/getAllBill.js
+++ server/controllers/getAllBill.js
@@ -1,22 +1,23 @@
 const { Bill } = require('../db-models/bill-model');
 
-const getAllBill = async (req, res,next) => {
-    try {
-      const allBill = await Bill.find()
-        // .populate({
-        //   path: "items",
-        //   populate: {
-        //     path: "itemDetail",
-        //     model: "Item",
-        //   },
-        // })
-        .sort({ createdAt: -1 })
-        .limit(Number(req.query.size));
-      const billCount = await Bill.countDocuments();
-      res.status(200).json({ message: { allBill, billCount } });
-    } catch (error) {
-      next(error)
-    }
-  };
+const getAllBill = async (req, res, next) => {
+  try {
+    const allBill = await Bill.find()
+      .populate({
+        path: 'items',
+        populate: {
+          path: 'itemDetail',
+          model: 'Item',
+          select: '-itemCostPricePerUnit',
+        },
+      })
+      .sort({ createdAt: -1 })
+      .limit(Number(req.query.size));
+    const billCount = await Bill.countDocuments();
+    res.status(200).json({ message: { allBill, billCount } });
+  } catch (error) {
+    next(error);
+  }
+};
 
-  module.exports = getAllBill;
+module.exports = getAllBill;
