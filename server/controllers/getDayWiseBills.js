5a6c672a810784b736cc23817d4d731ae2aa28c2
Yash priyam <yashpriyam74@gmail.com>
2023-05-13 14:21:25 +0530

    add limit of 30 to day wise bills

--- server/controllers/getDayWiseBills.js
+++ server/controllers/getDayWiseBills.js
@@ -1,50 +1,54 @@
 const { Bill } = require('../db-models/bill-model');
 const { DailyBill } = require('../db-models/dailybill-model');
 
-const getDayWiseBills = async (req, res,next) => {
-    try {
-      const allDailyBills = await Bill.aggregate([
-        {
-          $group: {
-            _id: { $dateToString: { format: '%Y-%m-%d', date: '$createdAt' } },
-            totalNumberOfBillsForToday: {
-              $sum: 1,
-            },
-            totalBillAmount: {
-              $sum: '$billAmountTotal',
-            },
-            totalMRPAmount: {
-              $sum: '$billMRPTotal',
-            },
-            totalDiscountAmount: {
-              $sum: '$billDiscountTotal',
-            },
-            totalItemBilled: {
-              $sum: '$totalNumberOfUniqueItems',
-            },
-            totalQuantityBilled: {
-              $sum: '$totalNumberOfItems',
-            },
-            totalDailyProfit: {
-              $sum: '$totalBillProfit',
-            },
-            totalCashPay: {
-              $sum: '$cashPay',
-            },
-            totalUpiPay: {
-              $sum: '$upiPay',
-            },
-            totalAmountReturn: {
-              $sum: '$amountReturn',
-            },
+const getDayWiseBills = async (req, res, next) => {
+  try {
+    const allDailyBills = await Bill.aggregate([
+      {
+        $group: {
+          _id: { $dateToString: { format: '%Y-%m-%d', date: '$createdAt' } },
+          totalNumberOfBillsForToday: {
+            $sum: 1,
+          },
+          totalBillAmount: {
+            $sum: '$billAmountTotal',
+          },
+          totalMRPAmount: {
+            $sum: '$billMRPTotal',
+          },
+          totalDiscountAmount: {
+            $sum: '$billDiscountTotal',
+          },
+          totalItemBilled: {
+            $sum: '$totalNumberOfUniqueItems',
+          },
+          totalQuantityBilled: {
+            $sum: '$totalNumberOfItems',
+          },
+          totalDailyProfit: {
+            $sum: '$totalBillProfit',
+          },
+          totalCashPay: {
+            $sum: '$cashPay',
+          },
+          totalUpiPay: {
+            $sum: '$upiPay',
+          },
+          totalAmountReturn: {
+            $sum: '$amountReturn',
+          },
+          dayBills: {
+            $push: '$$ROOT',
           },
         },
-      ]).sort({ _id: -1 });
-      const dailyBillCount = await DailyBill.countDocuments();
-      res.status(200).json({ message: { allDailyBills, dailyBillCount } });
-    } catch (error) {
-      next(error)
-    }
-  };
+      },
+    ])
+      .sort({ _id: -1 })
+      .limit(30);
+    res.status(200).json({ message: { allDailyBills } });
+  } catch (error) {
+    next(error);
+  }
+};
 
-  module.exports = getDayWiseBills;
+module.exports = getDayWiseBills;
