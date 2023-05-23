4e2dafe4343b6b11f2695bd9bc23c6422c49abb2
Yash priyam <yashpriyam74@gmail.com>
2023-05-23 14:33:11 +0530

    sort PO on getting

--- server/controllers/getOrdersByQuery.js
+++ server/controllers/getOrdersByQuery.js
@@ -1,14 +1,13 @@
 const PurchaseOrder = require('../db-models/purchase-order-model');
 
+const getOrdersByQuery = async (req, res, next) => {
+  try {
+    const { query } = req.body;
+    const orders = await PurchaseOrder.find(query).sort({ createdAt: -1 });
+    res.status(200).send({ message: 'orders found', orders });
+  } catch (error) {
+    next(error);
+  }
+};
 
-const getOrdersByQuery = async (req, res,next) => {
-    try {
-      const { query } = req.body;
-      const orders = await PurchaseOrder.find(query);
-      res.status(200).send({ message: 'orders found', orders });
-    } catch (error) {
-      next(error)
-    }
-  };
-
-  module.exports = getOrdersByQuery;
+module.exports = getOrdersByQuery;
