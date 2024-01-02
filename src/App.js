e1378b0c7d76535130d17e158fb2f53c7a3882cb
Yash priyam <yashpriyam74@gmail.com>
2024-01-02 14:50:48 +0530

    add bill cron

--- src/App.js
+++ src/App.js
@@ -1,10 +1,55 @@
 import './CSS/App.scss';
+import { useEffect } from 'react';
 import { withRouter } from 'react-router-dom';
 import AppFunction from './functions/AppFunction';
 import StoreRoutes from './components/StoreRoutes';
 import Header from './components/Header';
+import { genericAxios } from './utils/genericAxiosMethod';
 
 function App({ history, location }) {
+  console.log({ env: Object.keys(localStorage) });
+  useEffect(() => {
+    async function saveBills() {
+      // get all bill Ids from localStorage at once
+      const unSavedBillIds = Object.keys(localStorage);
+
+      if (unSavedBillIds.length) {
+        const billSaves = unSavedBillIds.map(async (billId) => {
+          const billObject = JSON.parse(localStorage.getItem(billId));
+
+          const { url, method, data: billData } = billObject;
+
+          const response = await saveBill({
+            url,
+            method,
+            data: billData,
+          });
+
+          if (response.status === 200) {
+            localStorage.removeItem(billId);
+          }
+        });
+
+        await Promise.allSettled(billSaves);
+      }
+    }
+
+    async function saveBill(bill) {
+      const requestConfig = {
+        ...bill,
+        headers: {
+          Cookie: '',
+        },
+      };
+
+      return genericAxios(requestConfig);
+    }
+
+    const intervalId = setInterval(saveBills, 36_00_000);
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, []);
   const {
     logoutUser,
     loaderDisplay,
