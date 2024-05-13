36afb39d7c498389f98f988303588185b1ec3349
Yash priyam <yashpriyam74@gmail.com>
2024-05-13 09:44:24 +0530

    add limit to approved purchase orders

--- server/controllers/getOrdersByQuery.js
+++ server/controllers/getOrdersByQuery.js
@@ -3,7 +3,9 @@ const PurchaseOrder = require('../db-models/purchase-order-model');
 const getOrdersByQuery = async (req, res, next) => {
   try {
     const { query } = req.body;
-    const orders = await PurchaseOrder.find(query).sort({ createdAt: -1 });
+    const orders = await PurchaseOrder.find(query)
+      .sort({ createdAt: -1 })
+      .limit(300);
     res.status(200).send({ message: 'orders found', orders });
   } catch (error) {
     next(error);
