b8f7d1f9a65afdb4b82cf73480bb3757d904e7f6
pinkeshroy <pinkeshyadav9661@gmail.com>
2024-09-03 12:51:19 +0530

    base url move to env

--- src/Pages/UnpaidPOs.jsx
+++ src/Pages/UnpaidPOs.jsx
@@ -1,19 +1,16 @@
-import "../CSS/unpaidPOs.css"
-import React, { useEffect, useRef, useState } from 'react'
+import '../CSS/unpaidPOs.css';
+import React, { useEffect, useRef, useState } from 'react';
 import { API_METHODS } from 'src/utils/constants/apiMethods';
 import { API_PATHS } from 'src/utils/constants/apiPaths';
 import { genericAxios } from 'src/utils/genericAxiosMethod';
-import { useHistory } from "react-router-dom";
-import {
-  WhatsappShareButton,
-  WhatsappIcon,
-} from 'react-share';
+import { useHistory } from 'react-router-dom';
+import { WhatsappShareButton, WhatsappIcon } from 'react-share';
 const UnpaidPOs = () => {
   const history = useHistory();
 
   const [unpaidStatusList, setUnpaidStatusList] = useState([]);
   const query = { isApproved: false, isDraft: false };
-
+  const baseUrl = process.env.REACT_APP_FRONTEND_BASE_URL;
   const getOrders = async () => {
     try {
       const response = await genericAxios({
@@ -34,54 +31,89 @@ const UnpaidPOs = () => {
   useEffect(() => {
     getOrders();
   }, []);
-  const handleOnClickOfSharePurchaseOrder = async ({event, paidStatus }) => {
+  const handleOnClickOfSharePurchaseOrder = async ({ event, paidStatus }) => {
     event.stopPropagation();
     event.preventDefault();
   };
   return (
     <div className="unpaidPOs-container">
       <h1>Unpaid POs</h1>
-      <table className='unpaidPOs-table'>
-        <thead className='unpaidPOs-table-thead'>
-          <tr className='unpaidPOs-table-thead-tr'>
-            <th className='unpaidPOs-table-thead-tr-th'>S.No</th>
-            <th className='unpaidPOs-table-thead-tr-th'>Dealer Name</th>
-            <th className='unpaidPOs-table-thead-tr-th'>Phone Number</th>
-            <th className='unpaidPOs-table-thead-tr-th'>Payment</th>
-            <th className='unpaidPOs-table-thead-tr-th'>Bill Amount</th>
-            <th className='unpaidPOs-table-thead-tr-th'>Paid Amount</th>
-            <th className='unpaidPOs-table-thead-tr-th'>Procurement Source</th>
-            <th className='unpaidPOs-table-thead-tr-th'>Created At</th>
-            <th className='unpaidPOs-table-thead-tr-th'>Remark</th>
-            <th className='unpaidPOs-table-thead-tr-th'>Status</th>
+      <table className="unpaidPOs-table">
+        <thead className="unpaidPOs-table-thead">
+          <tr className="unpaidPOs-table-thead-tr">
+            <th className="unpaidPOs-table-thead-tr-th">S.No</th>
+            <th className="unpaidPOs-table-thead-tr-th">Dealer Name</th>
+            <th className="unpaidPOs-table-thead-tr-th">Phone Number</th>
+            <th className="unpaidPOs-table-thead-tr-th">Payment</th>
+            <th className="unpaidPOs-table-thead-tr-th">Bill Amount</th>
+            <th className="unpaidPOs-table-thead-tr-th">Paid Amount</th>
+            <th className="unpaidPOs-table-thead-tr-th">Procurement Source</th>
+            <th className="unpaidPOs-table-thead-tr-th">Created At</th>
+            <th className="unpaidPOs-table-thead-tr-th">Remark</th>
+            <th className="unpaidPOs-table-thead-tr-th">Status</th>
           </tr>
         </thead>
-        <tbody className='unpaidPOs-table-tbody'>
+        <tbody className="unpaidPOs-table-tbody">
           {unpaidStatusList?.map((paidStatus, index) => (
-            <tr className={index%2===0?'unpaidPOs-table-tbody-tr-even':'unpaidPOs-table-tbody-tr-odd'}
-            key={index}
-            onClick={()=>{      
-              history.push(`/purchaseOrderBill/${paidStatus._id}`);
-            }}
+            <tr
+              className={
+                index % 2 === 0
+                  ? 'unpaidPOs-table-tbody-tr-even'
+                  : 'unpaidPOs-table-tbody-tr-odd'
+              }
+              key={index}
+              onClick={() => {
+                history.push(`/purchaseOrderBill/${paidStatus._id}`);
+              }}
             >
-              <td className='unpaidPOs-table-tbody-tr-td'>{index + 1}</td>
-              <td className='unpaidPOs-table-tbody-tr-td'>{paidStatus?.dealerName}</td>
-              <td className='unpaidPOs-table-tbody-tr-td'>{paidStatus?.phoneNumber}</td>
-              <td className='unpaidPOs-table-tbody-tr-td'>{paidStatus?.payment}</td>
-              <td className='unpaidPOs-table-tbody-tr-td'>{paidStatus?.billAmount}</td>
-              <td className='unpaidPOs-table-tbody-tr-td'>{paidStatus?.totalPaidAmount}</td>
-              <td className='unpaidPOs-table-tbody-tr-td'>{paidStatus?.procurementSource}</td>
-              <td className='unpaidPOs-table-tbody-tr-td'> {new Date(paidStatus?.createdAt).toUTCString()}</td>
-              <td className='unpaidPOs-table-tbody-tr-td'>{paidStatus?.remark ? paidStatus?.remark : '...'}</td>
-              <td className='unpaidPOs-table-tbody-tr-td' id ="td-button">
-                <div className=".unpaidPOs-table-tbody-tr-td-div" >
+              <td className="unpaidPOs-table-tbody-tr-td">{index + 1}</td>
+              <td className="unpaidPOs-table-tbody-tr-td">
+                {paidStatus?.dealerName}
+              </td>
+              <td className="unpaidPOs-table-tbody-tr-td">
+                {paidStatus?.phoneNumber}
+              </td>
+              <td className="unpaidPOs-table-tbody-tr-td">
+                {paidStatus?.payment}
+              </td>
+              <td className="unpaidPOs-table-tbody-tr-td">
+                {paidStatus?.billAmount}
+              </td>
+              <td className="unpaidPOs-table-tbody-tr-td">
+                {paidStatus?.totalPaidAmount}
+              </td>
+              <td className="unpaidPOs-table-tbody-tr-td">
+                {paidStatus?.procurementSource}
+              </td>
+              <td className="unpaidPOs-table-tbody-tr-td">
+                {' '}
+                {new Date(paidStatus?.createdAt).toUTCString()}
+              </td>
+              <td className="unpaidPOs-table-tbody-tr-td">
+                {paidStatus?.remark ? paidStatus?.remark : '...'}
+              </td>
+              <td className="unpaidPOs-table-tbody-tr-td" id="td-button">
+                <div className=".unpaidPOs-table-tbody-tr-td-div">
                   <p>{paidStatus?.isPaid ? 'Paid' : 'Unpaid'}</p>
-                  <button className=".unpaidPOs-table-tbody-tr-td-cover-button"
+                  <button
+                    className=".unpaidPOs-table-tbody-tr-td-cover-button"
                     onClick={(e) =>
-                      handleOnClickOfSharePurchaseOrder({event:e,paidStatus })
+                      handleOnClickOfSharePurchaseOrder({
+                        event: e,
+                        paidStatus,
+                      })
                     }
                   >
-                    <WhatsappShareButton url={`\nhttp://localhost:3000/purchaseOrderBill/${paidStatus?._id}\nPay purchase Order Bill:- \n${paidStatus.billPhotos[0]?.secure_url??""}`} title={"Pay purchase Order Bill:- "}>
+                    <WhatsappShareButton
+                      url={`\n${baseUrl}/purchaseOrderBill/${
+                        paidStatus?._id
+                      }\n${
+                        paidStatus?.billPhotos[0]?.secure_url
+                          ? `Pay purchase Order Bill:- \n${paidStatus.billPhotos[0].secure_url}`
+                          : ''
+                      }`}
+                      title={'Pay purchase Order Bill:- '}
+                    >
                       <WhatsappIcon size={32} round />
                     </WhatsappShareButton>
                   </button>
@@ -92,6 +124,6 @@ const UnpaidPOs = () => {
         </tbody>
       </table>
     </div>
-  )
-}
-export default UnpaidPOs
+  );
+};
+export default UnpaidPOs;
