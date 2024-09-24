59411e9d551f02b909c3928ef6a40a2d0ec77075
Rishabh Pillay <rishabhpillay03@gmail.com>
2024-09-24 10:31:17 +0530

    create-expired-item-schema

--- server/controllers/getExpiredItems.js
+++ server/controllers/getExpiredItems.js
@@ -6,7 +6,7 @@ const getExpiredItems = async (req, res, next) => {
         res.status(200).send({ message: 'Got all expired items', expiredItems });
     } catch (error) {
         console.error('Error failed to get expired items:', error);
-        res.status(500).send({ message: 'Failed to get expired items', error });
+        res.status(400).send({ message: 'Failed to get expired items', error });
     }
 };
 
