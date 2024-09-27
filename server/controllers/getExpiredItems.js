62253e4da1b6d9e1c175b34dc6471aaae0795e5f
Rishabh Pillay <rishabhpillay03@gmail.com>
2024-09-27 15:04:24 +0530

    create-expired-item-list-page

--- server/controllers/getExpiredItems.js
+++ server/controllers/getExpiredItems.js
@@ -1,10 +1,24 @@
 const { ExpiredItem } = require('../db-models/expired-item');
 
-const getExpiredItems = async (req, res, next) => {
+const getExpiredItems = async (req, res) => {
     try {
-        const expiredItems = await ExpiredItem.find({}).populate('itemId', '_id itemName itemBarcode'); 
+        const expiredItems = await ExpiredItem.find({}).populate('itemId', '_id itemName itemBarcode').lean();
+        const groupedItems = expiredItems.reduce((acc, item) => {
+            const itemId = item.itemId._id.toString();
 
-        res.status(200).send({ message: 'Got all expired items', expiredItems });
+            if (!acc[itemId]) {
+                acc[itemId] = { ...item, totalItems: item.totalItems };
+            } else {
+                acc[itemId].totalItems += item.totalItems;
+            }
+
+
+            return acc;
+        }, {});
+
+        const groupedExpiredItems = Object.values(groupedItems);
+
+        res.status(200).send({ message: 'Got all expired items', expiredItems: groupedExpiredItems });
     } catch (error) {
         console.error('Error failed to get expired items:', error);
         res.status(400).send({ message: 'Failed to get expired items', error });
--- server/routes/index.js
+++ server/routes/index.js
@@ -300,6 +300,6 @@ router.post(
   payPurchaseOrderBill
 );
 router.post(API_PATHS.EXPIRED_ITEM.ADD_EXPIRED_ITEM,isLoggedIn,addExpiredItem)
-router.get(API_PATHS.EXPIRED_ITEM.GET_EXPIRED_ITEMS,isLoggedIn,getExpiredItems)
+router.get(API_PATHS.EXPIRED_ITEM.GET_EXPIRED_ITEMS,getExpiredItems)
 
 module.exports = router;
