7bf372aea2996b895b9dea23c70a51eb86c52a9d
yashpriyam <yashpriyam74@gmail.com>
2024-11-26 22:51:45 +0530

    fix daily bills limit to 5

--- server/controllers/getDayWiseBills.js
+++ server/controllers/getDayWiseBills.js
@@ -44,7 +44,7 @@ const getDayWiseBills = async (req, res, next) => {
       },
     ])
       .sort({ _id: -1 })
-      .limit(15);
+      .limit(5);
     res.status(200).json({ message: { allDailyBills } });
   } catch (error) {
     next(error);
