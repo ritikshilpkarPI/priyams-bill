d112503ba888c3d99c17dc3ccef3d7bae10b2ec6
dhananjayadav <dhananjay2451@gmail.com>
2025-01-24 16:56:24 +0530

    PIPROD-1152: fix api payload for save or cache bill

--- src/Pages/newBillPage/NewBillPage.jsx
+++ src/Pages/newBillPage/NewBillPage.jsx
@@ -171,10 +171,8 @@ const NewBillPage = () => {
 
     try {
       const addBillResponse = await saveOrCacheBillAPI({
-        data: {
           ...billObject,
           billID: bill.billId
-        }
       });
       if (addBillResponse.isError) {
         setApiLoading(false);
