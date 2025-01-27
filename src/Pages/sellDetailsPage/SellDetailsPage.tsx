c9f8d45bafdcbf6e4481474f0a107b9273c12cc3
rishabhpillay <rishabhpillay03@gmail.com>
2025-01-27 17:17:19 +0530

    PIPROD-1131: add css to error message

--- src/Pages/sellDetailsPage/SellDetailsPage.tsx
+++ src/Pages/sellDetailsPage/SellDetailsPage.tsx
@@ -4,6 +4,7 @@ import { useParams } from 'react-router-dom';
 import { getCurrentAndPreviousYearDates } from '../../utils/getCurrentAndPreviousYearDates';
 import { LoadingOverlay, Text } from '@mantine/core';
 import { getItemsSellDetailsByPurchaseOrderIdAPI } from '../../utils/apiUtils';
+import './SellDetailsPage.css';
 
 const SellDetailsPage = () => {
   const { purchaseOrderId } = useParams<{ purchaseOrderId: string }>();
@@ -14,6 +15,7 @@ const SellDetailsPage = () => {
   const [endDate] = useState(dates.currentDate);
   const [timePeriod] = useState('monthly');
   const [loading, setLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
 
   const fetchDetails = async () => {
     if (
@@ -31,6 +33,7 @@ const SellDetailsPage = () => {
       );
       if (response?.isError) {
         setLoading(false);
+        setIsError(true);
       } else {
         setTableData(response.data);
         setLoading(false);
@@ -50,10 +53,16 @@ const SellDetailsPage = () => {
         visible={loading}
         overlayBlur={1}
       />
-      {tableData.length > 0 ? (
+      {isError ? (
+        <div className="error-text-container">
+          <Text size="lg">No data found.</Text>
+        </div>
+      ) : tableData.length > 0 ? (
         <SellDetailsTable tableData={tableData} />
       ) : (
-        <Text>Something went wrong.</Text>
+        <div className="error-text-container">
+          <Text size="lg">No data found.</Text>
+        </div>
       )}
     </div>
   );
