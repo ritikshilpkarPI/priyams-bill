63c166fe7ee733044340f6fa03d88a3a570394e1
rishabhpillay <rishabhpillay03@gmail.com>
2025-01-29 19:16:54 +0530

    PIPROD-1214: Refactored fetchTableDetails function

--- src/utils/getCurrentAndPreviousDates.ts
+++ src/utils/getCurrentAndPreviousDates.ts
@@ -1,16 +1,15 @@
+const convertDateToISO = (date: Date): string => {
+  return new Date(date.toLocaleString('en-US', { timeZone: 'Asia/Kolkata' }))
+    .toISOString()
+    .slice(0, 10);
+};
 export const getCurrentAndPreviousDates = (monthsAgo: number) => {
   const currentDate = new Date();
   const previousMonthDate = new Date();
-  previousMonthDate.setMonth(currentDate.getMonth() - monthsAgo); 
-
-  const formatDate = (date: Date): string => {
-    return new Date(date.toLocaleString('en-US', { timeZone: 'Asia/Kolkata' }))
-      .toISOString()
-      .slice(0, 10);
-  };
+  previousMonthDate.setMonth(currentDate.getMonth() - monthsAgo);
 
   return {
-    currentDate: formatDate(currentDate),
-    previousDate: formatDate(previousMonthDate),
+    currentDate: convertDateToISO(currentDate),
+    previousDate: convertDateToISO(previousMonthDate),
   };
-  };
+};
