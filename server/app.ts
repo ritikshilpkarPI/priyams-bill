5b868bb752df2d8ed9307a351d2aebf73e877432
dhananjayadav <whatswrongwithgithub@gmail.com>
2025-01-31 16:58:12 +0530

    PIPROD-1156: update netlify toml function

--- server/app.ts
+++ server/app.ts
@@ -29,7 +29,7 @@ app.use(
   })
 );
 
-app.use('/.netlify/functions/app', routers);
+app.use('/.netlify/functions/server', routers);
 
 const clientBuildPath = path.join(__dirname, '../../build');
 app.use(express.static(clientBuildPath));
