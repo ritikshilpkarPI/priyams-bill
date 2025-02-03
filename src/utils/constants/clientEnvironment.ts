f71ff11bc5c71c32e8abfcf668b95dd182bc6072
dhananjayadav <whatswrongwithgithub@gmail.com>
2025-02-03 12:35:49 +0530

    PIPROD-1159: update client env

--- src/utils/constants/clientEnvironment.ts
+++ src/utils/constants/clientEnvironment.ts
@@ -1,4 +1,4 @@
 export const CLIENT_ENVIRONMENT = Object.freeze({
   REACT_APP_API_BASE_URL: process.env.REACT_APP_API_BASE_URL || '',
-  REACT_APP_ENV: process.env.REACT_APP_API_BASE_URL || 'dev'
+  REACT_APP_ENV: process.env.REACT_APP_ENV || 'dev'
 });
