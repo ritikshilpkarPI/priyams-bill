225c25eac1a19ef1f82edad0b6e8cc88f87a02b1
dhananjayadav <whatswrongwithgithub@gmail.com>
2025-02-03 13:26:43 +0530

    PIPROD-1159: update netlify/functions server file

--- netlify/functions/server.js
+++ netlify/functions/server.js
@@ -1,7 +1,3 @@
-const serverless = require("serverless-http");
+const handler = require("../../dist/server/app.js").handler;
 
-const app = require("../../dist/server/app.js").app;
-
-module.exports.handler = serverless(app,{
-    framework: 'express'
-});
+module.exports.handler = handler;
