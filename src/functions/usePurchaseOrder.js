a87acab6dec0dd68d72d357e17be5a9abc929f81
dhananjayadav <whatswrongwithgithub@gmail.com>
2025-02-04 16:32:12 +0530

    PIPROD-1262: replace useHistory with useNavigate hook

--- src/functions/usePurchaseOrder.js
+++ src/functions/usePurchaseOrder.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
 import { useForm } from '@mantine/form';
 import useBarcodeSearchItems from './useBarcodeSearchItems';
-import { useHistory } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { useParams } from 'react-router-dom';
 import { genericAxios } from '../utils/genericAxiosMethod';
 import { API_PATHS } from '../utils/constants/apiPaths';
@@ -17,7 +17,7 @@ const usePurchaseOrder = (history) => {
   const [editIndex, setEditIndex] = useState(-1);
   const [slabs, setSlabs] = useState([]);
   const [isNotGetUpdated, setIsNotGetUpdated] = useState(true);
-  const locate = useHistory();
+  const navigate = useNavigate();
   const [cloudBills, setCloudBills] = useState([]);
   const [deleteBills, setDeleteBills] = useState([]);
   const [isEditable, setIsEditable] = useState(true);
@@ -197,7 +197,7 @@ const usePurchaseOrder = (history) => {
       if (id) {
         getDetails(id);
       } else {
-        locate.push(`/purchase/${_id}`);
+        navigate(`/purchase/${_id}`);
       }
     } catch (err) {
       offLoader();
@@ -313,7 +313,7 @@ const usePurchaseOrder = (history) => {
         setMessage({ success: true, failed: false, status });
         purchaseForm.reset();
         offLoader();
-        locate.push('/approval');
+        navigate('/approval');
       } else {
         setMessage({
           success: false,
@@ -429,7 +429,7 @@ const usePurchaseOrder = (history) => {
         const { _id } = order;
         offLoader();
         if (!id) {
-          locate.push(`/purchase/${_id}`);
+          navigate(`/purchase/${_id}`);
         } else {
           getDetails(id);
         }
