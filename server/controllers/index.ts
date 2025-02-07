53694f05fc001de9e3574f3c669fb3815a3308d0
priyankasahu1009 <priyanka1009sahu@gmail.com>
2025-02-07 16:47:51 +0530

    PIPROD-1260: revert unused changes

--- server/controllers/index.ts
+++ server/controllers/index.ts
@@ -83,7 +83,8 @@ import expelOrderToRider from './expelOrderToRider';
 import createRzpQRByAmount from './createRzpQRByAmount';
 import assignOrderToRider from './assignOrderToRider';
 import getItemsSellDetailsByPurchaseOrderId from './getItemsSellDetailsByPurchaseOrderId';
-import getItemsSellDetailsByItemId from "./getItemsSellDetailsByItemId";
+import getItemsSellDetailsByItemId from './getItemsSellDetailsByItemId';
+import createPurchaseOrderWithDealer from "./createPurchaseOrderWithDealer"
 
 export {
   getStaff,
@@ -172,4 +173,5 @@ export {
   assignOrderToRider,
   getItemsSellDetailsByPurchaseOrderId,
   getItemsSellDetailsByItemId,
+  createPurchaseOrderWithDealer
 };
