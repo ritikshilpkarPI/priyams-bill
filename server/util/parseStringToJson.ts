c5b4c0d9d343ab41439bdf0defe0145103bb3773
priyankasahu1009 <priyanka1009sahu@gmail.com>
2025-02-11 16:36:10 +0530

    PIPROD-1191:update parseStringToJson

--- server/util/parseStringToJson.ts
+++ server/util/parseStringToJson.ts
@@ -1,9 +1,11 @@
 export const parseStringToJson = (stringifiedJson: string): Record<string, any> => {
-    if (typeof stringifiedJson !== "string") return {};
+    if (typeof stringifiedJson !== "string") {
+        throw new Error("Invalid input: Expected a JSON string");
+    }
     try {
       return JSON.parse(stringifiedJson);
     } catch (err) {
-      return {};
+        throw new Error("Invalid JSON format");
     }
   };
   
