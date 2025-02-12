3c98017263d2182d82d1d4c3ab59cefee16e8544
dhananjayadav <whatswrongwithgithub@gmail.com>
2025-02-12 18:39:40 +0530

    PIPROD-1262: resolve username undef error

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -9,7 +9,7 @@ import { Loader } from '@mantine/core';
 
 
 const Login = () => {
-  const [username, setusername] = useState('');
+  const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [errorMsg, setErrorMsg] = useState('');
   const [loading, setLoading] = useState(false);
