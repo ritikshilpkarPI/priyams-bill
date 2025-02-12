8e25056623e42c8e03fbc4afbeb773cce4d1c789
dhananjayadav <whatswrongwithgithub@gmail.com>
2025-02-12 12:00:33 +0530

    PIPROD-1262: update headers config in api clint

--- src/configs/axiosConfig.ts
+++ src/configs/axiosConfig.ts
@@ -5,7 +5,9 @@ const apiClient: AxiosInstance = axios.create({
   baseURL: getServerBaseUrl(),
   timeout: 10000,
   headers: {
-    'Content-Type': 'application/json',
+    common: {
+      'Content-Type': 'application/json',
+    }
   },
 });
 
