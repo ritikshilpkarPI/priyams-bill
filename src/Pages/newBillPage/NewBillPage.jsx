cc3a63e1e8b1907ebf3d91b44c14d9f6a2777ec2
priyankasahu1009 <priyanka1009sahu@gmail.com>
2025-02-13 11:14:09 +0530

    PIPROD-1149: rename to fixedtotwodecimalplace

--- src/Pages/newBillPage/NewBillPage.jsx
+++ src/Pages/newBillPage/NewBillPage.jsx
@@ -14,7 +14,7 @@ import { BillItems } from '../../components/BillItems/BillItems';
 import { PaymentSection } from '../../components/PaymentSection/PaymentSection';
 import { useSelector } from "react-redux";
 import { selectItemsFeedData } from 'src/redux/allItemsFeedData/allItemsFeedDataSelector';
-import { roundToTwoDecimalPlaces } from 'src/utils/roundToTwoDecimalPlaces';
+import { fixedToTwoDecimalPlace } from 'src/utils/fixedToTwoDecimalPlace';
 
 
 const NewBillPage = () => {
@@ -132,8 +132,8 @@ const NewBillPage = () => {
             <PaymentSection
               cashPay={billState.cashPay}
               upiPay={billState.upiPay}
-              amountReturn={roundToTwoDecimalPlaces(billState.amountReturn)}
-              totalAmount={roundToTwoDecimalPlaces(billState.billAmountTotal)}
+              amountReturn={fixedToTwoDecimalPlace(billState.amountReturn)}
+              totalAmount={fixedToTwoDecimalPlace(billState.billAmountTotal)}
               onPaymentChange={updatePayment}
               onSubmit={handlePaymentSubmit}
               isLoading={isSubmitting}
--- src/utils/roundToTwoDecimalPlaces.ts
+++
@@ -1,7 +0,0 @@
-export const roundToTwoDecimalPlaces = (num: string | number): number => {
-  const parsedNum = typeof num === "string" ? parseFloat(num) : num;
-
-  if (isNaN(parsedNum)) return 0;
-
-  return Math.round(parsedNum * 100) / 100;
-};
