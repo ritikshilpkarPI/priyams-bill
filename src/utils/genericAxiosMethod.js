0c2f92e0a625cbbd4e68151a2a7bbe6794ec350c
dhananjayadav <whatswrongwithgithub@gmail.com>
2025-02-15 15:31:49 +0530

    PIPROD-1302: remove axios config and generic axios changes

--- src/utils/genericAxiosMethod.js
+++ src/utils/genericAxiosMethod.js
@@ -5,8 +5,7 @@ export const genericAxios = async ({
   method = 'get',
   data = {},
   headers = {
-    'Content-Type': 'application/json',
-    Cookie: 'token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzNDZjZjFiOTgzZTg1YmZiOTNlNTg4ZCIsIm5hbWUiOiJUZXN0IEFkbWluIiwicm9sZSI6ImFkbWluIiwidXNlcm5hbWUiOiJ0ZXN0X2FkbWluIiwiaWF0IjoxNzM5NTk2NTU2LCJleHAiOjE3NDIxODg1NTZ9.9mOY7RXYioi-9zmVyZZM6ZQxOfwL0J0K7XT6n74NkLs',
+    Cookie: '',
   },
 }) => {
   try {
@@ -15,10 +14,7 @@ export const genericAxios = async ({
       params,
       method,
       data,
-      headers: {
-        'Content-Type': 'application/json',
-        Cookie: 'token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzNDZjZjFiOTgzZTg1YmZiOTNlNTg4ZCIsIm5hbWUiOiJUZXN0IEFkbWluIiwicm9sZSI6ImFkbWluIiwidXNlcm5hbWUiOiJ0ZXN0X2FkbWluIiwiaWF0IjoxNzM5NTk2NTU2LCJleHAiOjE3NDIxODg1NTZ9.9mOY7RXYioi-9zmVyZZM6ZQxOfwL0J0K7XT6n74NkLs'
-      },
+      headers,
       withCredentials: true,
     });
     return result;
