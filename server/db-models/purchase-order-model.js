91ec9dd0a6a85763a3b8bbb93cd901feb7601935
dhananjayadav <whatswrongwithgithub@gmail.com>
2025-02-17 19:10:51 +0530

    PIPROD-1302: update item and purchase order model

--- server/db-models/purchase-order-model.js
+++ server/db-models/purchase-order-model.js
@@ -5,6 +5,7 @@ const purchaseOrderSchema = new mongoose.Schema({
     {
       barcode: String,
       inputName: String,
+      sku: String,
       stockQuantity: Number,
       minimumQuantity: Number,
       itemQuantity: Number,
@@ -18,6 +19,13 @@ const purchaseOrderSchema = new mongoose.Schema({
       item_id: String,
       brand: String,
       category: String,
+      subCategory: String,
+      flavourOrFeature: String,
+      freeItemsAvailable: Boolean,
+      returnPolicyAvailable: Boolean,
+      returnPolicyRemarks: String,
+      companyName: String,
+      saleTime: String,
       imageUrl: {
         public_id: String,
         secure_url: String
@@ -27,6 +35,8 @@ const purchaseOrderSchema = new mongoose.Schema({
         {
           date: Date,
           value: Number,
+          mfgDate: Date,
+          isShelfExpired: Boolean,
         },
       ],
       slabPrice: [],
