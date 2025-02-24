1e1e9ac82c0fb518354e86ccb1056090449c0f70
dhananjayadav <whatswrongwithgithub@gmail.com>
2025-02-24 17:40:52 +0530

    PIPROD-1336: enable field validation to allow points value in mrp cp and sp

--- src/utils/validations/draftItemFormValidation.ts
+++ src/utils/validations/draftItemFormValidation.ts
@@ -2,10 +2,10 @@ import * as Yup from "yup";
 export const draftItemFormValidation = Yup.object({
     barcode: Yup.string().trim().required('Barcode is required.'),
     inputName: Yup.string().trim().required('Item name is required.'),
-    itemQuantity: Yup.number().required().min(1, "Packet Qty. must be greater than 0"),
+    itemQuantity: Yup.number().required().min(1, "Packet Qty. must be greater or 1"),
     unit: Yup.string().required('Unit is required.'),
     mrp: Yup.number()
-      .min(1, 'MRP must be greater than 0')
+      .min(0.01, 'MRP must be greater than 0')
       .required('MRP is required.')
       .test('greater-than-selling-price', 'MRP must be greater or equal to SP.', function(value) {
         const { sellingPrice } = this.parent;
@@ -14,17 +14,17 @@ export const draftItemFormValidation = Yup.object({
     companyName: Yup.string().trim().required('Company Name is required.'),
     brand: Yup.string().trim().required('Brand Name is required.'),
     costPrice: Yup.number()
-      .min(1, 'Cost Price must be greater than 0.')
+      .min(0.01, 'Cost Price must be greater than 0.')
       .required('Cost Price is required.')
       .test('greater-than-zero', 'Cost Price must be greater than 0.', value => value > 0),
     sellingPrice: Yup.number()
-      .min(1, 'SP must be greater than 0')
+      .min(0.01, 'SP must be greater than 0')
       .required('Selling Price is required.')
       .test('greater-than-cost-price', 'SP must be greater or equal to CP.', function(value) {
         const { costPrice } = this.parent;
         return value >= costPrice;
       }),
     stockQuantity: Yup.number()
-      .min(1, 'Order Quantity must be greater than 0.')
+      .min(1, 'Order Quantity must be greater or 1.')
       .required('Order Quantity is required.')
   });
