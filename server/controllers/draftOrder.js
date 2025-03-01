8ffb629f8ff74a9962a72324898f31a2f5f3ecaa
yashaktivo <yash@aktivolabs.com>
2025-03-01 14:06:51 +0530

    PIPROD: 1336: remove consoles

--- server/controllers/draftOrder.js
+++ server/controllers/draftOrder.js
@@ -2,9 +2,7 @@ const PurchaseOrder = require('../db-models/purchase-order-model');
 
 const draftOrder = async (req, res,next) => {
     try {
-      console.log("drafting order");
       const { id } = req.body;
-      console.log({id});
       const order = await PurchaseOrder.findByIdAndUpdate(
         id,
         {
@@ -14,12 +12,10 @@ const draftOrder = async (req, res,next) => {
         },
         { new: true }
       );
-      console.log({order});
       res
         .status(200)
         .send({ message: 'order drafted successfully', success: true, order });
     } catch (error) {
-      console.log({error});
       next(error)
     }
   };
