a1fa792aa1cfc9ea77d757b5f2e61291fa1ef641
rishabhpillay <rishabhpillay03@gmail.com>
2025-03-03 14:11:58 +0530

    PIPROD-1377: create global CSS variables

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './CSS/index.css';
+import './styles/global.css';
 import reportWebVitals from './reportWebVitals';
 import { AppStateContextProvider } from './AppState/appState.context';
 import { MantineProvider } from '@mantine/core';
