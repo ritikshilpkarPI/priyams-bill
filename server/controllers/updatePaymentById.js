98764f35254d7648e548bd81000c9e4c7a84e3c2
rishabhpillay <rishabhpillay03@gmail.com>
2025-03-05 16:02:06 +0530

    PIPROD-1388: create function getDaysBetweenDates and uploadMultipleImages

--- server/controllers/updatePaymentById.js
+++ server/controllers/updatePaymentById.js
@@ -1,75 +1,100 @@
+const mongoose = require ('mongoose');
 const PurchaseOrder = require('../db-models/purchase-order-model');
-const { clodinaryFoldersPathKey } = require('../util/constant');
-const { uploadToCloudinary } = require('../util/image');
+const { uploadMultipleImages } = require('../util/image');
 const { parseStringToJson } = require('../util/parseStringToJson');
-
-const uploadPaymentImages = async (images) => {
-  return await Promise.all(
-    images.map(async (image) => {
-      const imagesUrl = await uploadToCloudinary(
-        image.data,
-        image.name,
-        clodinaryFoldersPathKey.bill
-      );
-      return {
-        public_id: imagesUrl.public_id,
-        secure_url: imagesUrl.secure_url,
-      };
-    })
-  );
-};
+const { convertDateToIST } = require('../util/convertDateToIST');
+const { getDaysBetweenDates } = require('../util/getDaysBetweenDates');
+const { MESSAGES } = require('../constants/messages');
 
 const updatePaymentById = async (req, res, next) => {
   try {
     const purchase_id = req.params.id;
-    const parsedData = parseStringToJson(req.body.data);
-    const { type, data } = parsedData;
-    const purchaseOrder = await PurchaseOrder.findById(purchase_id);
+     if (!mongoose.Types.ObjectId.isValid(purchase_id)){
+      return res
+        .status(404)
+        .json({ message: MESSAGES.INVALID_ID_FORMAT, success: false });
+     }
 
+    const { type, data }  = parseStringToJson(req.body.data);
+    if (!type || !data) {
+      return res
+      .status(404)
+      .json({ message: MESSAGES.MISSING_REQUIRED_FIELDS, success: false });
+    }
+
+    const { totalPayableAmount, totalBillAmount, paymentType } = data;
+    
+    if (!totalPayableAmount || !totalBillAmount || !paymentType) {
+      return res.status(400).json({
+        message: MESSAGES.MISSING_REQUIRED_FIELDS,
+        success: false,
+      });
+    }
+
+    if (!['credit', 'partiallypaid', 'fullypaid'].includes(paymentType.toLowerCase())) {
+      return res.status(400).json({
+        message: MESSAGES.INVALID_PAYMENT_TYPE,
+        success: false
+      });
+    }
+
+    const purchaseOrder = await PurchaseOrder.findById(purchase_id);
     if (!purchaseOrder) {
       return res
         .status(404)
-        .json({ message: 'Purchase order not found', success: false });
+        .json({ message: MESSAGES.PURCHASE_ORDER_NOT_FOUND, success: false });
     }
 
     let updatedPurchaseDetails = purchaseOrder.purchaseDetails || {};
 
     if (type.toLowerCase() === 'credit') {
-      const { creditAmount, payDate, creditLimitInDays } = data;
+      const { creditAmount, payDate } = data;
+      if (!creditAmount || !payDate ) {
+        return res
+        .status(404)
+        .json({ message: MESSAGES.MISSING_REQUIRED_FIELDS, success: false });
+      }
+      
+      const startDate = convertDateToIST(payDate).toISOString();
+      const endDate = convertDateToIST(new Date()).toISOString();
+      const creditLimitInDays = getDaysBetweenDates(startDate, endDate);
+      
       updatedPurchaseDetails.credits = [
         ...purchaseOrder.purchaseDetails.credits,
         {
           creditAmount,
           payDate,
           creditLimitInDays,
-          createdAt: new Date(),
         },
       ];
     } else if (type.toLowerCase() === 'payment') {
-      const { paidBy, paidAmount, paymentDate } = data;
+      const { paidBy, paidAmount } = data;
+      if (!paidBy || !paidAmount ) {
+        return res
+        .status(404)
+        .json({ message: MESSAGES.MISSING_REQUIRED_FIELDS, success: false });
+      }
       if (
         ['upi', 'cheque', 'neft'].includes(paidBy.toLowerCase()) &&
         !  req.files
       ) {
         return res
           .status(400)
-          .json({ message: 'Bill image is required', success: false });
+          .json({ message: MESSAGES.PAYMENT_IMAGES_REQUIRED, success: false });
       }
-      let paymentImgURL
+
+      let paymentImgURL;
       if(req.files){
-        const { paymentImges } = req.files;
-        paymentImgURL = paymentImges ? await uploadPaymentImages(paymentImges) : [];
+        const { paymentImages } = req.files;
+        paymentImgURL = paymentImages ? await uploadMultipleImages(paymentImages) : [];
       }
       
-
       updatedPurchaseDetails.payments = [
         ...purchaseOrder.purchaseDetails.payments,
         {
           paidBy,
           paymentImgURL,
           paidAmount: parseFloat(paidAmount).toFixed(2),
-          paymentDate: paymentDate || new Date(),
-          createdAt: new Date(),
         },
       ];
     }
@@ -77,8 +102,6 @@ const updatePaymentById = async (req, res, next) => {
     const totalPaidAmount = updatedPurchaseDetails.payments
       .reduce((total, payment) => total + Number(payment.paidAmount), 0)
       .toFixed(2);
-
-    const { totalPayableAmount, totalBillAmount, paymentType } = data;
 
     const updatedOrder = await PurchaseOrder.findByIdAndUpdate(
       purchase_id,
@@ -93,7 +116,7 @@ const updatePaymentById = async (req, res, next) => {
       { new: true }
     );
     res.status(200).send({
-      message: 'order updated successfully',
+      message: MESSAGES.ORDER_UPDATED_SUCCESSFULLY,
       success: true,
       order: updatedOrder,
       updatedOrder,
