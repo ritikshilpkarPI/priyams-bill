65153a4719aeef491c75393e8abd07ff00d37b35
rishabhpillay <rishabhpillay03@gmail.com>
2025-03-07 16:38:28 +0530

    PIPROD-1388: add constants into constants file

--- server/constants/constants.ts
+++ server/constants/constants.ts
@@ -2,4 +2,10 @@ export const CONSTANTS = Object.freeze({
     REJECT: "reject",
     APPROVE: "approve",
     DRAFT: "draft",
+    UPDATE_PAYMENT_BY_ID:{
+        CREDIT: 'credit',
+        PAYMENT: 'payment',
+        PAYMENT_TYPES: ['credit', 'partiallypaid', 'fullypaid'] ,
+        PAID_BY: ['upi', 'cheque', 'neft']
+    }
 })
