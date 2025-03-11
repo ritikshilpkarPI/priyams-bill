eedc10d98ad3262968c2af204f007203f49d233b
rishabhpillay <rishabhpillay03@gmail.com>
2025-03-11 12:51:23 +0530

    PIPROD-1355: remove PaymentDetailTable

--- src/components/paymentDetailsPanel/PaymentDetailsPanel.tsx
+++ src/components/paymentDetailsPanel/PaymentDetailsPanel.tsx
@@ -3,9 +3,7 @@ import { Box, Flex } from '@mantine/core';
 import { useSelector } from 'react-redux';
 import { selectPurchaseOrder } from '../../redux/purchaseOrder/purchaseOrderSelectors';
 import { PaymentDetailsForm } from '../paymentDetailsForm/PaymentDetailsForm';
-import { PaymentDetailTable } from '../paymentDetailTable/PaymentDetailTable';
 import ShareOnWhatsApp from '../shareOnWhatsApp';
-import { AddCreditForm } from '../addCreditForm/AddCreditForm';
 
 export const PaymentDetailAndBillPanel = () => {
   const purchaseOrder = useSelector(selectPurchaseOrder);
@@ -16,11 +14,6 @@ export const PaymentDetailAndBillPanel = () => {
      <Flex direction="column">
       <PaymentDetailsForm
         purchaseOrderId={purchaseOrder?._id}
-        // paymentDetailIdx={(purchaseOrder?.purchaseDetails?.length || 0) + 2}
-      />
-      <PaymentDetailTable
-        purchaseOrderId={purchaseOrder?._id}
-        totalPaidAmount={purchaseOrder?.totalPaidAmount || 0}
       />
     </Flex>
     <Box mt="16px">
