4135e6bad508f59fb8be706c88c6087ab6e2f81c
Ritik Shilpkar <ritikshilpkar@Ritiks-MacBook-Air.local>
2025-03-26 12:51:28 +0530

    PIPROD-1509: make page responsive

--- src/components/InventoryItemPanel.tsx
+++ src/components/InventoryItemPanel.tsx
@@ -1,5 +1,5 @@
-import { Table, NumberInput, ActionIcon, Text } from '@mantine/core';
-// import { IconTrash } from '@tabler/icons';
+import React from 'react';
+import { Table, NumberInput, ActionIcon, Text, ScrollArea } from '@mantine/core';
 
 interface InventoryItemPanelProps {
   items: any[];
@@ -8,39 +8,41 @@ interface InventoryItemPanelProps {
 }
 
 export const InventoryItemPanel: React.FC<InventoryItemPanelProps> = ({ items, onQuantityChange, onRemoveItem }) => {
-    
   return (
-    <Table striped highlightOnHover withBorder>
-      <thead>
-        <tr>
-          <th>Item Name</th>
-          <th>Barcode</th>
-          <th>Current Stock</th>
-          <th>Quantity to Add</th>
-          <th>Action</th>
-        </tr>
-      </thead>
-      <tbody>
-        {items.map((item) => (
-          <tr key={item.itemDetail._id}>
-            <td>{item.itemDetail.itemName}</td>
-            <td>{item.itemDetail.itemBarcode}</td>
-            <td>{item.itemDetail.itemStockQuantity}</td>
-            <td>
-              <NumberInput
-                value={item.quantityToAdd}
-                onChange={(value) => onQuantityChange(item.itemDetail._id, value || 0)}
-                min={1}
-              />
-            </td>
-            <td>
-              <ActionIcon color="red" onClick={() => onRemoveItem(item.itemDetail._id)}>
-                {/* <IconTrash size={16} /> */} trash
-              </ActionIcon>
-            </td>
+    <ScrollArea style={{ width: '100%' }}>
+      <Table striped highlightOnHover withBorder>
+        <thead>
+          <tr>
+            <th>Item Name</th>
+            <th>Barcode</th>
+            <th>Current Stock</th>
+            <th>Quantity to Add</th>
+            <th>Action</th>
           </tr>
-        ))}
-      </tbody>
-    </Table>
+        </thead>
+        <tbody>
+          {items.map((item) => (
+            <tr key={item.itemDetail._id}>
+              <td>{item.itemDetail.itemName}</td>
+              <td>{item.itemDetail.itemBarcode}</td>
+              <td>{item.itemDetail.itemStockQuantity}</td>
+              <td>
+                <NumberInput
+                  value={item.quantityToAdd}
+                  onChange={(value) => onQuantityChange(item.itemDetail._id, value || 0)}
+                  min={1}
+                />
+              </td>
+              <td>
+                <ActionIcon color="red" onClick={() => onRemoveItem(item.itemDetail._id)}>
+                  {/* Replace with an actual icon if available */}
+                  <Text size="sm">trash</Text>
+                </ActionIcon>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+    </ScrollArea>
   );
 };
