4c6d2bf403cc5287f62db6cf637098267634e2d4
priyankasahu1009 <priyanka1009sahu@gmail.com>
2025-03-27 18:32:27 +0530

    feature/PIPROD-1485:  removed unused changes

--- src/utils/purchaseOrderValidations.ts
+++ src/utils/purchaseOrderValidations.ts
@@ -1,7 +1,7 @@
 import * as Yup from 'yup';
 import { dealerFormValidation } from './validations/dealerFormValidation';
 import { draftItemFormValidation } from './validations/draftItemFormValidation';
-import { PaymentCoverageComplete, paymentDetailFormValidation } from './validations/paymentDetailFormValidation';
+import { paymentDetailFormValidation } from './validations/paymentDetailFormValidation';
 
 
 export const validateDealerDetails = async (purchaseOrder: PurchaseOrderDataType) => {
@@ -28,7 +28,6 @@ export  const validatePaymentDetails = async (
   ) => {
     try {
       await paymentDetailFormValidation.validate(purchaseOrder.purchaseDetails);
-      await PaymentCoverageComplete.validate(purchaseOrder.purchaseDetails);
       return true;
     } catch (err) {
       return false;
