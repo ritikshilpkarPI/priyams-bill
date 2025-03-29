e9e24f772cf96d83437752bd7445ef6edd8784db
Obliger0 <sujalgotharwal904@gmail.com>
2025-03-29 13:40:37 +0530

    PIPROD-1525: Add profitPercentage fieldName in PO and show in item table

--- src/types.d.ts
+++ src/types.d.ts
@@ -269,6 +269,7 @@ declare global {
     inputName?: string;
     freeItemsRemarks?: string;
     itemHasExpiry?: boolean | null;
+    profitPercentage?: number;
   }
 
   interface PurchasedItemDetailFormProps {
