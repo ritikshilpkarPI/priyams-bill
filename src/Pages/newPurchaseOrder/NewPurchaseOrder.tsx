694069a170ff3a075ed7ab6a9521e1f9a1e60681
priyankasahu1009 <priyanka1009sahu@gmail.com>
2025-04-01 11:47:17 +0530

    feature/PIPROD-1535 revert unused file

--- src/Pages/newPurchaseOrder/NewPurchaseOrder.tsx
+++ src/Pages/newPurchaseOrder/NewPurchaseOrder.tsx
@@ -45,7 +45,6 @@ const NewPurchaseOrder = () => {
   const [loading, setLoading] = useState(false);
   const purchaseOrder = useSelector(selectPurchaseOrder);
   const { isApproved = false} = purchaseOrder;
-  const [isApprovedPO, setIsApprovedPO] = useState(isApproved);
 
   const purchaseOrderStatusInfo = useSelector(selectPurchaseOrderStatusInfo);
   const purchaseOrderStatus = (() => {
