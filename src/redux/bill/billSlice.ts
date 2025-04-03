bf8bdc7dab0448f291db5011ac0ada62e3240fb3
rishabhpillay <rishabhpillay03@gmail.com>
2025-04-03 17:57:22 +0530

    PIPROD-1543: resolve conflict

--- src/redux/bill/billSlice.ts
+++ src/redux/bill/billSlice.ts
@@ -40,11 +40,11 @@ const billSlice = createSlice({
     resetBillState: (state) => {
       Object.assign(state, initialState, { billId: `${uuidv4()}-${Date.now()}` });
     },
-    updateStaff: (state, action:PayloadAction<string>) => {
+    updateStaffInBill: (state, action:PayloadAction<string>) => {
       state.staffId = action.payload;
     }
   },
 });
 
-export const { updateBillItems, updateCustomerInfo, updatePayment, resetBillState,setBill, updateStaff } = billSlice.actions;
+export const { updateBillItems, updateCustomerInfo, updatePayment, resetBillState,setBill, updateStaffInBill } = billSlice.actions;
 export default billSlice.reducer;
