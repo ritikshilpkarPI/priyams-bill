24d600875eedf9d526875e4aab8690f3b2403d27
priyankasahu1009 <priyanka1009sahu@gmail.com>
2025-04-04 11:54:11 +0530

    feature/PIPROD-1549 show eror message if storedata is present

--- src/utils/apiUtils.ts
+++ src/utils/apiUtils.ts
@@ -1,6 +1,8 @@
+import { toast } from 'react-toastify';
 import { getAPI, postAPI } from './apiMethods';
 import { API_PATHS } from './constants/apiPaths';
 import { getUserDeviceInfo } from './getUserDeviceInfo';
+import MESSAGES from './constants/messages';
 
 
 
@@ -20,9 +22,12 @@ export const getBillingLeanItemsAPI = async () => {
 export const saveOrCacheBillAPI = async (data: SaveBillAPIDataType) => {
   try {
     const storeData = localStorage.getItem('storeData');
+    if (!storeData) {
+      return toast.error(MESSAGES.STOREDATA_IS_REQUIRED); // Show toast
+    }
     const parsedStoreData = storeData ? JSON.parse(storeData) : null;
     const requestData = { ...data, storeData:parsedStoreData };
-
+   
     const response = await postAPI({
       path: API_PATHS.BILLING.SAVE_OR_CACHE_BILL,
       data: requestData,
