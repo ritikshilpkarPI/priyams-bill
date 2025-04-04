48141a2592e7d21483f2948bab1f94d786aef8eb
rishabhpillay <rishabhpillay03@gmail.com>
2025-04-04 14:56:35 +0530

    PIPROD-1559: put DealerState into type.ts file

--- src/types.d.ts
+++ src/types.d.ts
@@ -428,6 +428,19 @@ declare global {
     latitude?: number;
     longitude?: number;
   }
+  interface Dealer {
+    _id?: string;
+    dealerName: string;
+    dealerBrands: string[];
+    dealerCompanies: string[];
+    dealerNumber: number;
+  }
+  interface DealerState {
+    dealers: Dealer[];
+    loading: boolean;
+    error: string | null;
+    selectedDealerId?: string;
+  }
 }
 declare module '*.scss' {
   const content: { [className: string]: string };
