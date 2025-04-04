1429bcf46388d93f812eae38d057f7ed9d5de591
Ritik Shilpkar <ritikshilpkar@Ritiks-MacBook-Air.local>
2025-04-04 15:55:35 +0530

    PIPROD-1559: add timestamps

--- server/db-models/brand-model.ts
+++ server/db-models/brand-model.ts
@@ -4,6 +4,10 @@ import { BrandType } from 'server/types';
 const BrandSchema: Schema<BrandType> = new Schema({
   brandName: { type: String, unique: true },
   companyId: { type: Types.ObjectId, ref: 'Company'}
-});
+},
+{
+  timestamps: true
+}
+);
 
 export const BrandModel = mongoose.model<BrandType>('Brand', BrandSchema);
