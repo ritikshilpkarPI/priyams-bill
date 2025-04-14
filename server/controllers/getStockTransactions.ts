054f08784b25b6a68f2e9c5fa1ee8d5de57d65ef
priyankasahu1009 <priyanka1009sahu@gmail.com>
2025-04-14 16:03:22 +0530

    PIPROD-1625 update getStockTransactions

--- server/controllers/getStockTransactions.ts
+++ server/controllers/getStockTransactions.ts
@@ -1,28 +1,26 @@
 import { NextFunction, Request, Response } from "express";
 import mongoose from "mongoose";
 import { StockTransactionModel } from "../db-models/stock-transaction-model";
-import { StoreModel } from "../db-models/store-model";
 import { MESSAGES } from "../constants/messages";
 
 export const getStockTransactions = async (req: Request, res: Response,next: NextFunction) => {
   try {
-    const { transactionId, pincode } = req.query;
+    const { transactionId, storeId } = req.query;
     const filter: Record<string, any> = {};
 
     if (transactionId && mongoose.Types.ObjectId.isValid(transactionId as string)) {
       filter._id = transactionId;
     }
 
-    if (pincode) {
-      const store = await StoreModel.findOne({ pincode });
-      if (!store) {
-        return res.status(400).json({ success: false, message: MESSAGES.STORE_NOT_FOUND_FOR_GIVEN_PINCODE});
-      }
-
-      filter.$or = [
-        { "source.sourceEntityId": store._id },
-        { "destination.destinationEntityId": store._id }
-      ];
+    if (storeId && mongoose.Types.ObjectId.isValid(storeId as string)) {
+        const storeObjectId = new mongoose.Types.ObjectId(storeId as string);
+  
+        filter.$or = [
+          { "source.sourceEntityId": storeObjectId },
+          { "destination.destinationEntityId": storeObjectId }
+        ];
+    } else if (storeId) {
+        return res.status(400).json({ success: false, message: MESSAGES.INVALID_STORE_ID });
     }
 
     const transactions = await StockTransactionModel.find(filter)
