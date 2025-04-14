007e8debf40a7a2464ba409efd7ec83df0422ff3
Ritik Shilpkar <ritikshilpkar@Ritiks-MacBook-Air.local>
2025-04-14 18:28:28 +0530

    PIPROD-1561: add missing changes

--- server/controllers/getAllBill.js
+++ server/controllers/getAllBill.js
@@ -1,6 +1,7 @@
 const { Bill } = require('../db-models/bill-model');
 
 const getAllBill = async (req, res, next) => {
+  const { storeId } = req.query;
   try {
     const allBill = await Bill.find({
       ...(storeId && { storeId }),
