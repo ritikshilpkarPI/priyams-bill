e80026fe561033cff8cdb128875ed237d6611efc
Obliger0 <sujalgotharwal904@gmail.com>
2025-04-17 16:36:29 +0530

    Replace Item name with SKU in report UI (excluding PO)

--- src/Pages/Report.jsx
+++ src/Pages/Report.jsx
@@ -255,6 +255,9 @@ const showBillTable = (reportResult, selectedFilter) => (
             <Text align="center">Sl. No.</Text>
           </th>
           <th>
+            <Text align="center">SKU</Text>
+          </th>
+          <th>
             <Text align="center">Item Name</Text>
           </th>
           <th><Text align="center">Barcode</Text></th>
@@ -350,6 +353,12 @@ const TableRow = ({ itemBill, idx, filterName }) => {
         <td>
           <Text color="black" weight={500}>
             {idx + 1}
+          </Text>
+        </td>
+        <td>
+          <Text color="black" weight={500}>
+            {itemBill.items[0]?.itemDetail?.sku ||
+              itemBill.items.itemDetail?.sku || "N/A"}
           </Text>
         </td>
         <td>
