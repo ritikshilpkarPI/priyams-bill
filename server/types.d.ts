23941cb4a63c93d48ef27579068d985db457c044
priyankasahu1009 <priyanka1009sahu@gmail.com>
2025-04-17 11:16:05 +0530

    PIPROD-1625 requested changes

--- server/types.d.ts
+++ server/types.d.ts
@@ -144,3 +144,13 @@ export interface StockTransactionType extends Document {
 export interface StockTransactionsInterface {
   transactions: StockTransactionType[]
 }
+
+export interface StockTransactionFilterBody {
+  transactionId?: string[];
+  storeId?: string;
+  itemId?: string[];
+  startDate?: string;
+  endDate?: string;
+  page?: string;
+  limit?: string;
+}
--- src/utils/generateSlug.ts
+++ src/utils/generateSlug.ts
@@ -1,5 +1,5 @@
 export const generateSlug = (data: string) => {
-  const trimmedData = data.trim(); // Trim any leading/trailing spaces
+  const trimmedData = data.trim(); 
   const slug = trimmedData
     .toLowerCase()            
     .replace(/[^a-z0-9-]/g, '-') 
