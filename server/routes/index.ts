d28c5e2964c398fe4ae55d4feb189fd7fed73aa0
Obliger0 <sujalgotharwal904@gmail.com>
2025-04-23 12:10:57 +0530

    PIPROD-1665: Make requested changes

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -280,6 +280,6 @@ router.get(`${API_PATHS.ITEMS.GET_ITEMS_STATIC_FIELDS}/:id?`, itemsStaticAttribu
 router.get(API_PATHS.DEALER.GET_ALL_DEALERS, isLoggedIn, getAllDealers);
 
 router.post(API_PATHS.DEALER.ADD_NEW_DEALER, isLoggedIn, addNewDealer);
-router.post(API_PATHS.EXPIRED_ITEM.CREATE_EXPIRED_ITEM_BATCH,isLoggedIn, createExpiryItemsBatch);
+router.post(API_PATHS.EXPIRED_ITEM.CREATE_EXPIRED_ITEMS_BATCH, createExpiryItemsBatch);
 
 export default router;
