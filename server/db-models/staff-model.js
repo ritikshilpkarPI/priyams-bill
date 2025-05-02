9c72851495cd23196a92af7af5b66236c6e5ec64
Obliger0 <sujalgotharwal904@gmail.com>
2025-05-02 16:01:48 +0530

    PIPROD-1696: Add staffTypeEnum

--- server/db-models/staff-model.js
+++ server/db-models/staff-model.js
@@ -6,6 +6,11 @@ const ROLES = {
   STAFF: 'staff',
 };
 
+const staffTypeEnum = {
+  STORE: "STORE",
+  WAREHOUSE: "WAREHOUSE"
+};
+     
 const staffSchema = new mongoose.Schema({
   name: {
     type: String,
@@ -21,13 +26,24 @@ const staffSchema = new mongoose.Schema({
   password: {
     type: String,
   },
-  allowedRoutes: { 
-    type: [String]
+  allowedRoutes: {
+    type: [String],
+  },
+  staffType: {
+    type: String,
+    enum: staffTypeEnum
   },
 });
 staffSchema.methods.getJwtToken = function () {
   return jwt.sign(
-    { id: this._id, name: this.name, role: this.role, username: this.username, allowedRoutes: this.allowedRoutes },
+    {
+      id: this._id,
+      name: this.name,
+      role: this.role,
+      username: this.username,
+      allowedRoutes: this.allowedRoutes,
+      staffType: this.staffType
+    },
     process.env.JWT_SECRET,
     {
       expiresIn: `${process.env.JWT_EXPIRY}`,
