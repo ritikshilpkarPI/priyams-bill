7ce40b323ab05e945799b9581e30cb874e26f662
Rishabh Pillay <rishabhpillay@Sujals-MacBook-Air.local>
2025-05-10 12:27:50 +0530

    PIPROD-1723: fetch current date bill as default

--- src/utils/apiUtils.ts
+++ src/utils/apiUtils.ts
@@ -787,3 +787,21 @@ export const getStaffByToken = async ()=>{
     url: API_PATHS.STAFF.GET_STAFF_BY_TOKEN,
   })
 };
+
+export const getBillFeedAPI = async (
+  page?: number,
+  size?: number,
+  startDate?: string,
+  endDate?: string,
+  storeId?: string
+) => {
+  const path = `${API_PATHS.BILLING.GET_BILL_FEED}?page=${page || ''}&size=${size || ''}&startDate=${startDate || ''
+    }&endDate=${endDate || ''}&storeId=${storeId || ''}`;
+
+  try {
+    const response = await getAPI({ path });
+    return response;
+  } catch (error) {
+    return { isError: true, error };
+  }
+};
