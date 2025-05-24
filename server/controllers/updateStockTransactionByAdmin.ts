07d9479cc9a6fcb77f5252cfbbff170e1c503c48
rishabhpillay <rishabhpillay03@gmail.com>
2025-05-24 10:09:05 +0530

    PIPROD-1734: add approve by admin Chip on table

--- server/controllers/updateStockTransactionByAdmin.ts
+++ server/controllers/updateStockTransactionByAdmin.ts
@@ -181,10 +181,14 @@ export const updateStockTransactionByAdmin = async (
 
       if (adminRemark !== undefined) {
         transaction.adminRemark = adminRemark;
-        transaction.transactionStatus = CONSTANTS.TRANSACTIONS_STATUS.DESTINATION_UPDATED;
       }
-  
-  
+
+    if (!updateData.approvedByAdmin) {
+      transaction.transactionStatus = CONSTANTS.TRANSACTIONS_STATUS.APPROVED;
+    } else {
+      transaction.transactionStatus =
+        CONSTANTS.TRANSACTIONS_STATUS.DESTINATION_UPDATED;
+    }
 
     await transaction.save();
 
