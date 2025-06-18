ef408e85d552fbc6d2b3e410788b988070a8aa31
rishabhpillay <rishabhpillay03@gmail.com>
2025-06-18 12:47:06 +0530

    PIPROD-1811: Add CP on Store Inventory page

--- server/constants/constants.ts
+++ server/constants/constants.ts
@@ -10,7 +10,7 @@ export const CONSTANTS = Object.freeze({
     UPI: "upi",
     ADD: "ADD",
     WAREHOUSE: "WAREHOUSE",
-    STATIC_FIELDS_TO_SELECT: "itemName itemMRPperUnit itemBarcode itemPerUnitQuantity quantityUnitName itemCategory subCategory itemBrandName companyName flavourOrFeature itemHasExpiry saleTime images sku itemShelfDates itemShelfDates ",
+    STATIC_FIELDS_TO_SELECT: "itemName itemMRPperUnit itemBarcode itemPerUnitQuantity quantityUnitName itemCategory subCategory itemBrandName companyName flavourOrFeature itemHasExpiry saleTime images sku itemShelfDates itemShelfDates itemCostPricePerUnit ",
     REMOVE: "REMOVE",
     STORE: "STORE",
     DEALER: "DEALER",  
